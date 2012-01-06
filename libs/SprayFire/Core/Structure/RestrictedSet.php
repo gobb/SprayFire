d0e559cce99b1c0701264eb2d4d049c677e6c580
Charles Sprayberry <cspray@gmail.com>
2012-01-06 16:41:20 -0500

    ADD RestrictedSet a structure that gets unit tests running

---
+++ libs/SprayFire/Core/Structure/RestrictedSet.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * @brief A file holding a class that holds a set of objects; restricted to a specific
+ * type.
+ *
+ * @details
+ * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
+ * want to make simple, secure, dynamic website content.
+ *
+ * SprayFire repository: http://www.github.com/cspray/SprayFire/
+ *
+ * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
+ *
+ * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
+ *
+ * SprayFire is released under the Open-Source Initiative MIT license.
+ * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
+ *
+ * @author Charles Sprayberry cspray at gmail dot com
+ * @copyright Copyright (c) 2011, Charles Sprayberry
+ */
+
+namespace SprayFire\Core\Structure;
+
+/**
+ * @brief This class will hold a collection of unique objects and ensure that those
+ * objects are of a specific interface or class.
+ *
+ * @uses ReflectionClass
+ * @uses InvalidArgumentException
+ * @uses SprayFire.Core.Structure.GenericSet
+ * @uses SprayFire.Core.ObjectTypeValidator
+ * @uses SprayFire.Exception.TypeNotFoundException
+ */
+class RestrictedSet extends \SprayFire\Core\Structure\GenericSet {
+
+
+
+
+}
