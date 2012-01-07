67d5270d3c77bcb9d25c054360f392886151b9c9
Charles Sprayberry <cspray@gmail.com>
2012-01-07 14:49:59 -0500

    UPD * removing erroneous details from file block

--- libs/SprayFire/Bootstrap/PathGeneratorBootstrap.php
+++ libs/SprayFire/Bootstrap/PathGeneratorBootstrap.php
@@ -4,22 +4,6 @@
  * @file
  * @brief Holds a class responsible for setting up a SprayFire.Core.PathGenerator
  * implementation used to work with the directory structure.
- *
- * @details
- * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
- * want to make simple, secure, dynamic website content.
- *
- * SprayFire repository: http://www.github.com/cspray/SprayFire/
- *
- * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
- *
- * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
- *
- * SprayFire is released under the Open-Source Initiative MIT license.
- * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
- *
- * @author Charles Sprayberry cspray at gmail dot com
- * @copyright Copyright (c) 2011,2012 Charles Sprayberry
  */
 
 namespace SprayFire\Bootstrap;
@@ -69,7 +53,6 @@ namespace SprayFire\Bootstrap;
  * - <code>/web/index.php</code>
  *
  * @uses SprayFire.Bootstrap.Bootstrapper
- * @uses SprayFire.Core.PathGenerator
  * @uses SprayFire.Core.Directory
  */
 class PathGeneratorBootstrap implements \SprayFire\Bootstrap\Bootstrapper {
--- libs/SprayFire/Config/JsonConfig.php
+++ libs/SprayFire/Config/JsonConfig.php
@@ -4,22 +4,6 @@
  * @file
  * @brief Holds a SprayFire.Config.Configuration object that will convert
  * a JSON configuration file into a chainable ImmutableStorage object.
- *
- * @details
- * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
- * want to make simple, secure, dynamic website content.
- *
- * SprayFire repository: http://www.github.com/cspray/SprayFire/
- *
- * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
- *
- * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
- *
- * SprayFire is released under the Open-Source Initiative MIT license.
- * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
- *
- * @author Charles Sprayberry cspray at gmail dot com
- * @copyright Copyright (c) 2011,2012 Charles Sprayberry
  */
 
 namespace SprayFire\Config;
