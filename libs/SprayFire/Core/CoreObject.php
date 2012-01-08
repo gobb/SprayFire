ed34ca7d7112574fdacc082e5e85e1387655a338
Charles Sprayberry <cspray@gmail.com>
2012-01-07 23:51:57 -0500

    UPD CoreObject & SystemLogger fixing docs

--- libs/SprayFire/Core/CoreObject.php
+++ libs/SprayFire/Core/CoreObject.php
@@ -3,22 +3,6 @@
 /**
  * @file
  * @brief The framework's implementation of the libs.sprayfire.core.Object interface.
- *
- * @details
- * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
- * want to make simple, secure, dynamic website content.
- *
- * SprayFire repository: http://www.github.com/cspray/SprayFire/
- *
- * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
- *
- * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
- *
- * SprayFire is released under the Open-Source Initiative MIT license.
- * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
- *
- * @author Charles Sprayberry cspray at gmail dot com
- * @copyright Copyright (c) 2011,2012 Charles Sprayberry
  */
 
 namespace SprayFire\Core;
