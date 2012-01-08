d9d5b938c03d788eb99c9f69ccbfc98ec8750c5f
Charles Sprayberry <cspray@gmail.com>
2012-01-08 00:58:43 -0500

    UPD SprayFireRouter removing erroneous file docs

--- libs/SprayFire/Request/Router/SprayFireRouter.php
+++ libs/SprayFire/Request/Router/SprayFireRouter.php
@@ -5,22 +5,6 @@
  * @brief The framework's implementation of the SprayFire.Request.Router.Router interface
  * that allows for a Uri to be parsed and converted into a SprayFire.Request.Router.RoutedUri
  * based on a SprayFire.Config.Configuration object injected at construction.
- *
- * @details
- * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
- * want to make simple, secure, dynamic website content.
- *
- * SprayFire repository: http://www.github.com/cspray/SprayFire/
- *
- * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
- *
- * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
- *
- * SprayFire is released under the Open-Source Initiative MIT license.
- * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
- *
- * @author Charles Sprayberry cspray at gmail dot com
- * @copyright Copyright (c) 2011, Charles Sprayberry
  */
 
 namespace SprayFire\Request\Router;
