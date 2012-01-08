1953d0cd46d7b3a9cd690d76ebc06a84c9210c58
Charles Sprayberry <cspray@gmail.com>
2012-01-07 19:13:05 -0500

    UPD Logger/CoreObject removing erroneous file docs

--- libs/SprayFire/Logger/CoreObject.php
+++ libs/SprayFire/Logger/CoreObject.php
@@ -4,23 +4,8 @@
  * @file
  * @brief Holds an extension of SprayFire.Core.CoreObject to provide a single method
  * to log various messages.
- *
- * @details
- * SprayFire is a fully unit-tested, light-weight PHP framework for developers who
- * want to make simple, secure, dynamic website content.
- *
- * SprayFire repository: http://www.github.com/cspray/SprayFire/
- *
- * SprayFire wiki: http://www.github.com/cspray/SprayFire/wiki/
- *
- * SprayFire API Documentation: http://www.cspray.github.com/SprayFire/
- *
- * SprayFire is released under the Open-Source Initiative MIT license.
- * OSI MIT License <http://www.opensource.org/licenses/mit-license.php>
- *
- * @author Charles Sprayberry cspray at gmail dot com
- * @copyright Copyright (c) 2011, Charles Sprayberry
  */
+
 namespace SprayFire\Logger;
 
 /**
