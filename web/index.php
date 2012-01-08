afa7432cb7f02401e9132ddb44b3523a806aece7
Charles Sprayberry <cspray@gmail.com>
2012-01-08 01:11:34 -0500

    UPD web/index.php removing comments from source and moving profiler info down further to get a more accurate reading

--- web/index.php
+++ web/index.php
@@ -1,98 +1,91 @@
 <?php
-///**
-// * @file
-// * @brief The primary intialization script for SprayFire
-// */
-//
-//$requestStartTime = \microtime(true);
-//
-///**
-// * @var $installPath the directory libs, app and web directories are stored in
-// */
-//$installPath = \dirname(__DIR__);
-//
-///**
-// * @var $libsPath the directory the SprayFire framework and 3rd-party libs should
-// *      be stored.
-// */
-//$libsPath = $installPath . '/libs';
-//
-///**
-// * @var $SprayFireContainer
-// */
-//$SprayFireContainer = include $libsPath  . '/SprayFire/Bootstrap/bootstrap.php';
-//
-///**
-// * @todo The following markup eventually needs to be moved into the default
-// * template for HtmlResponder.
-// */
-//
-//// NOTE: The below code is a temporary measure until the templating system is in place
-//
-//$PathGenerator = $SprayFireContainer->getObject('PathGenerator');
-//
-//$styleCss = $PathGenerator->getUrlPath('css','sprayfire.style.css');
-//$sprayFireLogo = $PathGenerator->getUrlPath('images', 'sprayfire-logo-bar-75.png');
-//
-//
-//if ($SprayFireContainer->getObject('PrimaryConfig')->app->{'development-mode'} === 'on') {
-//    $errors = 'Errors: ' . \print_r($SprayFireContainer->getObject('ErrorHandler')->getTrappedErrors(), true);
-//    $memUsage = 'Memory usage: ' . \memory_get_peak_usage() / (1000*1024) . ' mb';
-//    $runTime = 'Execution time: ' . (\microtime(true) - $requestStartTime) . ' seconds';
-//    $debugInfo = <<<HTML
-//            <div id="debug-info" style="margin-top:1em;border:2px solid black;padding:5px;font-family:monospace;">
-//                <ul>
-//                    <li>$errors</li>
-//                    <li>$memUsage</li>
-//                    <li>$runTime</li>
-//                </ul>
-//            </div>
-//HTML;
-//
-//} else {
-//    $debugInfo = '';
-//}
-//
-//echo <<<HTML
-//<!DOCTYPE html>
-//    <html>
-//        <head>
-//            <title>Welcome to SprayFire!</title>
-//            <link href="{$styleCss}" rel="stylesheet" type="text/css" />
-//        </head>
-//        <body>
-//            <div id="content">
-//                <div id="header">
-//                    <h1><img src="{$sprayFireLogo}" id="sprayfire-logo" alt="SprayFire logo" width="200" height="75" /></h1>
-//                    <ul>
-//                        <li>ver: {$PrimaryConfig->SprayFire->version}</li>
-//                        <li>repo: <a href="http://www.github.com/cspray/SprayFire">http://www.github.com/cspray/SprayFire/</a></li>
-//                        <li>wiki: <a href="http://www.github.com/cspray/SprayFire/wiki/">http://www.github.com/cspray/SprayFire/wiki/</a></li>
-//                        <li>api docs: coming soon!</li>
-//
-//                    </ul>
-//                </div>
-//
-//                <div id="body">
-//                    <div id="main-content">
-//                    </div>
-//                </div>
-//
-//                <div id="footer">
-//                    <p style="text-align:center;"><span class="sprayfire-orange">Spray</span><span class="sprayfire-red">Fire</span> &copy; Charles Sprayberry 2011</p>
-//                </div>
-//            </div>
-//            {$debugInfo}
-//        </body>
-//    </html>
-//HTML;
+/**
+ * @file
+ * @brief The primary intialization script for SprayFire
+ */
 
+$requestStartTime = \microtime(true);
 
-include \dirname(__DIR__) . '/libs/SprayFire/Core/ClassLoader.php';
+/**
+ * @var $installPath the directory libs, app and web directories are stored in
+ */
+$installPath = \dirname(__DIR__);
 
-$ClassLoader = new \SprayFire\Core\ClassLoader();
-$ClassLoader->registerNamespaceDirectory('SprayFire', \dirname(__DIR__) . '/libs');
-\spl_autoload_register(array($ClassLoader, 'load'));
+/**
+ * @var $libsPath the directory the SprayFire framework and 3rd-party libs should
+ *      be stored.
+ */
+$libsPath = $installPath . '/libs';
 
-$SystemLogger = new \SprayFire\Logger\SystemLogger();
-\var_dump($SystemLogger->log(LOG_ERR, 'A test of the system log again from SystemLogger::log()'));
+/**
+ * @var $SprayFireContainer
+ */
+$SprayFireContainer = include $libsPath  . '/SprayFire/Bootstrap/bootstrap.php';
+
+/**
+ * @todo The following markup eventually needs to be moved into the default
+ * template for HtmlResponder.
+ */
+
+// NOTE: The below code is a temporary measure until the templating system is in place
+
+$PathGenerator = $SprayFireContainer->getObject('PathGenerator');
+$PrimaryConfig = $SprayFireContainer->getObject('PrimaryConfig');
+
+$styleCss = $PathGenerator->getUrlPath('css','sprayfire.style.css');
+$sprayFireLogo = $PathGenerator->getUrlPath('images', 'sprayfire-logo-bar-75.png');
+
+echo <<<HTML
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Welcome to SprayFire!</title>
+            <link href="{$styleCss}" rel="stylesheet" type="text/css" />
+        </head>
+        <body>
+            <div id="content">
+                <div id="header">
+                    <h1><img src="{$sprayFireLogo}" id="sprayfire-logo" alt="SprayFire logo" width="200" height="75" /></h1>
+                    <ul>
+                        <li>ver: {$PrimaryConfig->SprayFire->version}</li>
+                        <li>repo: <a href="http://www.github.com/cspray/SprayFire">http://www.github.com/cspray/SprayFire/</a></li>
+                        <li>wiki: <a href="http://www.github.com/cspray/SprayFire/wiki/">http://www.github.com/cspray/SprayFire/wiki/</a></li>
+                        <li>api docs: coming soon!</li>
+
+                    </ul>
+                </div>
+
+                <div id="body">
+                    <div id="main-content">
+                    </div>
+                </div>
+
+                <div id="footer">
+                    <p style="text-align:center;"><span class="sprayfire-orange">Spray</span><span class="sprayfire-red">Fire</span> &copy; Charles Sprayberry 2011</p>
+                </div>
+            </div>
+HTML;
+
+if ($PrimaryConfig->app->{'development-mode'} === 'on') {
+    $errors = 'Errors: ' . \print_r($SprayFireContainer->getObject('ErrorHandler')->getTrappedErrors(), true);
+    $memUsage = 'Memory usage: ' . \memory_get_peak_usage() / (1000*1024) . ' mb';
+    $runTime = 'Execution time: ' . (\microtime(true) - $requestStartTime) . ' seconds';
+    $debugInfo = <<<HTML
+            <div id="debug-info" style="margin-top:1em;border:2px solid black;padding:5px;font-family:monospace;">
+                <ul>
+                    <li>$errors</li>
+                    <li>$memUsage</li>
+                    <li>$runTime</li>
+                </ul>
+            </div>
+HTML;
+
+} else {
+    $debugInfo = '';
+}
+
+echo <<<HTML
+    {$debugInfo}
+    </body>
+</html>
+HTML;
