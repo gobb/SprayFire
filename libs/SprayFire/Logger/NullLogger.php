c1c8b7b5abe5912b7635a4b9e532a9544e943735
Charles Sprayberry <cspray@gmail.com>
2012-01-09 09:10:52 -0500

    UPD NullLogger adding docs

--- libs/SprayFire/Logger/NullLogger.php
+++ libs/SprayFire/Logger/NullLogger.php
@@ -2,16 +2,22 @@
 
 /**
  * @file
- * @brief
+ * @brief A SprayFire.Logger.Logger implementation implementing the NullObject
+ * design pattern.
  */
 
 namespace SprayFire\Logger;
 
 /**
- * @brief
+ * @brief This SprayFire.Logger.Logger will not actually log messages to any medium;
+ * logged messages are simply ignored.
  */
 class NullLogger extends \SprayFire\Core\CoreObject implements \SprayFire\Logger\Logger {
 
+    /**
+     * @param $message The message to log
+     * @return Always returns true
+     */
     public function log($message) {
         return true;
     }
