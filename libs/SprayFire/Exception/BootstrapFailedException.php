a4ecb660a0066ab48169513d616f2cf3bf0aac8c
Charles Sprayberry <cspray@gmail.com>
2012-01-09 09:14:08 -0500

    UPD BootstrapFailedException adding docs

--- libs/SprayFire/Exception/BootstrapFailedException.php
+++ libs/SprayFire/Exception/BootstrapFailedException.php
@@ -2,14 +2,12 @@
 
 /**
  * @file
- * @brief
+ * @brief An exception thrown if a SprayFire.Bootstrap.Bootstrapper could not
+ * successfully complete its bootstrapping procedures.
  */
 
 namespace SprayFire\Exception;
 
-/**
- * @brief
- */
 class BootstrapFailedException extends \RuntimeException {
 
 }
