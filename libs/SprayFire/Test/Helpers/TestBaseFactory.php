7cb8c93a3705cc822d3c2c6f2182aae2c13508d0
Charles Sprayberry <cspray@gmail.com>
2012-01-10 18:20:28 -0500

    ADD TestBaseFactory a basic extension of base factory that exposes protected methods for testing

---
+++ libs/SprayFire/Test/Helpers/TestBaseFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ */
+
+namespace SprayFire\Test\Helpers;
+
+/**
+ * @brief
+ */
+class TestBaseFactory extends \SprayFire\Factory\BaseFactory {
+
+    public function makeObject($className, array $options = array()) {
+        return $this->createObject($className, $options);
+    }
+
+    public function testGetFinalBlueprint($className, array $options = array()) {
+        return $this->getFinalBlueprint($className, $options);
+    }
+
+    public function testReplaceDotsWithSlashes($className) {
+        return $this->replaceDotsWithBackSlashes($className);
+    }
+
+}
