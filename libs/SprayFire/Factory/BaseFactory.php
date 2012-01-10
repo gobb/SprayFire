c35c94111e891f13c013dc8792499789b79a6623
Charles Sprayberry <cspray@gmail.com>
2012-01-10 15:22:01 -0500

    UPD BaseFactory changing the implementation details to maybe not suck so much

--- libs/SprayFire/Factory/BaseFactory.php
+++ libs/SprayFire/Factory/BaseFactory.php
@@ -37,9 +37,22 @@ abstract class BaseFactory extends \SprayFire\Core\Util\CoreObject implements \S
     protected $blueprints = array();
 
     /**
-     * @property $nullPrototype The name of the class to use for NullObject on this factory
+     * @property $ReflectedNullPrototype The name of the class to use for NullObject on this factory
      */
-    protected $nullPrototype;
+    protected $NullObject;
+
+    public function __construct($nullPrototype) {
+        $NullObject = $nullPrototype;
+        if (!is_object($NullObject)) {
+            try {
+                $ReflectedNullPrototype = new \ReflectionClass($nullPrototype);
+                $NullObject = $ReflectedNullPrototype->newInstance();
+            } catch (\ReflectionException $ReflectExc) {
+                throw new \SprayFire\Exception\FactoryConstructionException('The given NullObject prototype, ' . $nullPrototype . ', could not be loaded.', null, $ReflectExc);
+            }
+        }
+        $this->NullObject = $NullObject;
+    }
 
     /**
      * @brief Note \a $defaultOptions MUST be passed; if your class does not have
@@ -47,7 +60,11 @@ abstract class BaseFactory extends \SprayFire\Core\Util\CoreObject implements \S
      *
      * @details
      * Note that if you pass a key that already exists in the blueprint store you
-     * will override whatever default options were previously set there.
+     * will override whatever default options were previously set there.  Due to
+     * the way that array_merge works it is *HIGHLY RECOMMENDED* that you use a
+     * string key, regardless of whether that string is really a number.  Using a
+     * numeric key for options WILL result in far too many and/or the incorrect
+     * parameters being passed to object constructors.
      *
      * @internal
      * Although you can pass either a Java-style or PHP-style namespaced class
@@ -62,25 +79,105 @@ abstract class BaseFactory extends \SprayFire\Core\Util\CoreObject implements \S
     }
 
     /**
-     * @brief The \a $className passed will be used as the prototype when a requested
-     * Object could not be created and a NullObject should be returned instead.
-     *
-     * @param $className A Java or PHP-styled namespaced class
+     * @param $className A Java-style or PHP-style namespaced class
+     * @return An array storing a blueprint if the key has one associated or an
+     *         empty array if no blueprint exists.
      */
-    public function setNullPrototype($className) {
-        $this->nullPrototype = $this->replaceDotsWithBackSlashes($className);
+    public function getBlueprint($className) {
+        $bluePrintKey = $this->replaceDotsWithBackSlashes($className);
+        if (\array_key_exists($bluePrintKey, $this->blueprints) && \is_array($this->blueprints[$bluePrintKey])) {
+            return $this->blueprints[$bluePrintKey];
+        }
+        return array();
+    }
+
+    /**
+     * @brief
+     *
+     * @param $className A Java-stye or PHP-style namespaced class
+     * @param $options An array of options to be used when creating this object.
+     */
+    protected function createObject($className, array $options) {
+        try {
+            $className = $this->replaceDotsWithBackSlashes($className);
+            $options = $this->getFinalBlueprint($className, $options);
+            $ReflectedClass = new \ReflectionClass($className);
+            $returnObject = $ReflectedClass->newInstanceArgs($options);
+        } catch (\ReflectionException $ReflectExc) {
+            $returnObject = clone $this->NullObject;
+        }
+        return $returnObject;
+    }
+
+    /**
+     * @brief This method will take an array of options for a specific object,
+     * replace any NULL values with those values stored in \a $blueprints, given
+     * that the \a $className has a blueprint associated with it.
+     *
+     * @internal
+     * We are not using array_merge here for a reason.  We want users to be able
+     * to use any kind of index they would like for their options arrays.  If we
+     * used array_merge users would have to ensure that the keys for the blueprint
+     * and the options always matched up.  Ultimately this sounds really tedious
+     * and not something we want to enforce upon the user.  For now we will simply
+     * use the below alorithm to determine the final blueprint to be used.  If you
+     * feel that this algorithm can be approved upon please raise an issue at
+     * {@link http://www.github.com/cspray/issues/}
+     *
+     * @param $className The PHP-style namespaced class
+     * @param array $options
+     */
+    protected function getFinalBlueprint($className, array $options) {
+        $storedBlueprint = $this->getBlueprint($className);
+        if (empty($storedBlueprint)) {
+            return $options;
+        }
+
+        $blueprintCount = \count($storedBlueprint);
+        $optionsCount = \count($options);
+        $appendedOptions = null;
+
+        if ($optionsCount !== $blueprintCount) {
+            if ($optionsCount > $blueprintCount) {
+                $appendedOptions = \array_splice($options, $blueprintCount);
+            } else {
+                $options = \array_pad($options, $blueprintCount, null);
+            }
+        }
+
+        $i = 0;
+        $finalBlueprint = array();
+
+        for ($i; $i < $blueprintCount; $i++) {
+            $blueprintVal = $storedBlueprint[$i];
+            $optionVal = $options[$i];
+            $indexVal = $blueprintVal;
+            if (!is_null($optionVal)) {
+                $indexVal = $optionVal;
+            }
+            $finalBlueprint[] = $indexVal;
+        }
+
+        if (!empty($appendedOptions)) {
+            foreach ($appendedOptions as $val) {
+                $finalBlueprint[] = $val;
+            }
+        }
+
+        return $finalBlueprint;
     }
 
     /**
      * @param $dotSeparatedClass A Java-style namespaced class
      * @return A PHP-style namespaced class
      */
-    protected function replaceDotsWithBackSlashes($dotSeparatedClass) {
-        $dotSeparator = '.';
-        $slashSeparator = '\\';
-        $slashedClassName = \str_replace($dotSeparator, $slashSeparator, $dotSeparatedClass);
-        $trimmedClassName = \trim($slashedClassName, '\\ ');
-        return '\\' . $trimmedClassName;
+    protected function replaceDotsWithBackSlashes($className) {
+        $backSlash = '\\';
+        $dot = '.';
+        if (\strpos($className, $dot) !== false) {
+            $className = \str_replace($dot, $backSlash, $dotSeparatedClass);
+        }
+        return $backSlash . \trim($className, '\\ ');
     }
 
 }
