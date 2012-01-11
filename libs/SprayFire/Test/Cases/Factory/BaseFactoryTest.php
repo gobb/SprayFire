a1049ae2362cf927e5a7937cf53dea6c1db545f9
Charles Sprayberry <cspray@gmail.com>
2012-01-10 20:09:44 -0500

    UPD BaseFactoryTest updating tests to something more realistic for an expected implementation

--- libs/SprayFire/Test/Cases/Factory/BaseFactoryTest.php
+++ libs/SprayFire/Test/Cases/Factory/BaseFactoryTest.php
@@ -15,7 +15,7 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
     public function testSwappingJavaStyleNamesToPHPStyleNames() {
         $javaClass = 'SprayFire.Core.Util.CoreObject';
         $phpClass = '\\SprayFire\\Core\\Util\\CoreObject';
-        $Factory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass());
+        $Factory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass());
 
         $this->assertSame($phpClass, $Factory->testReplaceDotsWithSlashes($javaClass));
     }
@@ -28,7 +28,7 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
         $blueprintTwo = array('1', '2');
         $blueprintThree = array('one', 'two', 'three');
 
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass);
         $TestFactory->storeBlueprint($classOne, $blueprintOne);
         $TestFactory->storeBlueprint($classTwo, $blueprintTwo);
         $TestFactory->storeBlueprint($classThree, $blueprintThree);
@@ -39,12 +39,12 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGettingEmptyBlueprint() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass);
         $this->assertSame(array(), $TestFactory->getBlueprint('SprayFire.NonExistent.Class'));
     }
 
     public function testGettingFinalBlueprintWithNoOptions() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass);
 
         $className = 'SprayFire.Test.Class';
         $defaultBlueprint = array('arg1', 'arg2');
@@ -57,7 +57,7 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGettingFinalBlueprintReplacingAllDefaultOptions() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass);
 
         $className = 'SprayFire.Test.Class';
         $defaultBlueprint = array('arg1', 'arg2');
@@ -69,14 +69,14 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGettingFinalBlueprintWithNoStoredBlueprint() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass);
         $className = 'SprayFire.Test.Class';
         $specificOptions = array('arg1', 'arg2');
         $this->assertSame($specificOptions, $TestFactory->testGetFinalBlueprint($className, $specificOptions));
     }
 
     public function testGettingFinalBlueprintWithMoreOptionsThenDefaultBlueprint() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', new \stdClass);
         $className = 'SprayFire.Test.Class';
         $TestFactory->storeBlueprint($className, array(1, 2));
         $specificOptions = array(null, null, 3, 4, 5);
@@ -86,14 +86,14 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testCreatingATestObject() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('SprayFire.Test.Helpers.TestObject', new \SprayFire\Test\Helpers\TestObject());
         $className = 'SprayFire.Test.Helpers.TestObject';
         $Object = $TestFactory->makeObject($className);
         $this->assertTrue($Object instanceof \SprayFire\Test\Helpers\TestObject);
     }
 
     public function testCreatingATestObjectWithDefaultBlueprintOnly() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory(new \stdClass);
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('SprayFire.Core.Util.CoreObject', new \SprayFire\Test\Helpers\TestObject());
         $className = 'SprayFire.Test.Helpers.TestFactoryObject';
         $defaultBlueprint = array('param1', 'param2');
         $TestFactory->storeBlueprint($className, $defaultBlueprint);
@@ -103,15 +103,35 @@ class BaseFactoryTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testGettingANullObjectOnErrorPassingStringNameAsNullPrototype() {
-        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass');
+        $TestFactory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', 'stdClass');
         $className = 'SprayFire.Test.Class';
         $Object = $TestFactory->makeObject($className);
         $this->assertTrue($Object instanceof \stdClass);
     }
 
     public function testPassingInvalidNullPrototype() {
-        $Factory = new \SprayFire\Test\Helpers\TestBaseFactory('SprayFire.NonExistent');
-        $Object = $Factory->makeObject('SprayFire.NonExistent.Either');
+        $exceptionThrown = false;
+        try {
+            $Factory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', 'SprayFire.NonExistent');
+        } catch (\InvalidArgumentException $InvalArgExc) {
+            $exceptionThrown = true;
+        }
+        $this->assertTrue($exceptionThrown);
+    }
+
+    public function testPassingInvalidReturnType() {
+        $exceptionThrown = false;
+        try {
+            $Factory = new \SprayFire\Test\Helpers\TestBaseFactory('SprayFire.NonExistent', 'stdClass');
+        } catch (\SprayFire\Exception\TypeNotFoundException $InvalArgExc) {
+            $exceptionThrown = true;
+        }
+        $this->assertTrue($exceptionThrown);
+    }
+
+    public function testCreatingObjectNotProperReturnType() {
+        $Factory = new \SprayFire\Test\Helpers\TestBaseFactory('stdClass', 'stdClass');
+        $Object = $Factory->makeObject('SprayFire.Test.Helpers.TestObject');
         $this->assertTrue($Object instanceof \stdClass);
     }
 
