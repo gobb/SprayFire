2fd1ef1a5e1f8fccc9f3850218e90618dcf54bfa
Charles Sprayberry <cspray@gmail.com>
2012-01-12 16:48:14 -0500

    UPD BootstrapDataTest adding tests for an invalid configuration passed

--- libs/SprayFire/Test/Cases/Bootstrap/BootstrapDataTest.php
+++ libs/SprayFire/Test/Cases/Bootstrap/BootstrapDataTest.php
@@ -12,7 +12,7 @@ namespace SprayFire\Test\Cases\Bootstrap;
  */
 class BootstrapDataTest extends \PHPUnit_Framework_TestCase {
 
-    public function testBootstrapData() {
+    public function testValidBootstrapData() {
         $installPath = \SPRAYFIRE_ROOT;
         $libsPath = $installPath . '/libs';
         $configPath = $installPath . '/config';
@@ -29,29 +29,37 @@ class BootstrapDataTest extends \PHPUnit_Framework_TestCase {
         $actualDirectoryBootstrapData = $BootstrapData->PathGenBootstrap;
 
         $expectedIniBootstrapData = array();
-        $expectedIniBootstrapData['allow_url_fopen'] = 0;
-        $expectedIniBootstrapData['allow_url_include'] = 0;
-        $expectedIniBootstrapData['asp_tags'] = 0;
-        $expectedIniBootstrapData['date.timezone'] = 'America/New_York';
-        $expectedIniBootstrapData['default_charset'] = 'UTF-8';
-        $expectedIniBootstrapData['default_mimetype'] = 'text/html';
-        $expectedIniBootstrapData['assert.active'] = 0;
-        $expectedIniBootstrapData['magic_quotes_gpc'] = 0;
-        $expectedIniBootstrapData['magic_quotes_runtime'] = 0;
-        $expectedIniBootstrapData['expose_php'] = 0;
-        $expectedIniBootstrapData['display_errors'] = 1;
-        $expectedIniBootstrapData['display_startup_errors'] = 1;
-        $expectedIniBootstrapData['error_reporting'] = -1;
+        $expectedIniBootstrapData['global'] = array();
+        $expectedIniBootstrapData['global']['allow_url_fopen'] = 0;
+        $expectedIniBootstrapData['global']['allow_url_include'] = 0;
+        $expectedIniBootstrapData['global']['asp_tags'] = 0;
+        $expectedIniBootstrapData['global']['date.timezone'] = 'America/New_York';
+        $expectedIniBootstrapData['global']['default_charset'] = 'UTF-8';
+        $expectedIniBootstrapData['global']['default_mimetype'] = 'text/html';
+        $expectedIniBootstrapData['global']['assert.active'] = 0;
+        $expectedIniBootstrapData['global']['magic_quotes_gpc'] = 0;
+        $expectedIniBootstrapData['global']['magic_quotes_runtime'] = 0;
+        $expectedIniBootstrapData['global']['expose_php'] = 0;
+
+        $expectedIniBootstrapData['production'] = array();
+        $expectedIniBootstrapData['production']['display_errors'] = 0;
+        $expectedIniBootstrapData['production']['display_startup_errors'] = 0;
+        $expectedIniBootstrapData['production']['error_reporting'] = \E_ALL & ~\E_NOTICE;
+
+        $expectedIniBootstrapData['development'] = array();
+        $expectedIniBootstrapData['development']['display_errors'] = 1;
+        $expectedIniBootstrapData['development']['display_startup_errors'] = 1;
+        $expectedIniBootstrapData['development']['error_reporting'] = -1;
         $actualIniBootstrapData = $BootstrapData->IniBootstrap;
 
         $expectedConfigBootstrapData = array();
-        $expectedConfigBootstrapData['sprayFireConfig']['file'] = $configPath . '/json/sprayfire-configuration.json';
+        $expectedConfigBootstrapData['sprayFireConfig']['file'] = array('json', 'sprayfire-configuration.json');
         $expectedConfigBootstrapData['sprayFireConfig']['object'] = 'SprayFire.Config.JsonConfig';
         $expectedConfigBootstrapData['sprayFireConfig']['map-key'] = 'SprayFireConfig';
-        $expectedConfigBootstrapData['routesConfig']['file'] = $configPath . '/json/routes.json';
+        $expectedConfigBootstrapData['routesConfig']['file'] = array('json', 'routes.json');
         $expectedConfigBootstrapData['routesConfig']['object'] = 'SprayFire.Config.JsonConfig';
         $expectedConfigBootstrapData['routesConfig']['map-key'] = 'RoutesConfig';
-        $expectedConfigBootstrapData['pluginsConfig']['file'] = $configPath . '/json/plugins.json';
+        $expectedConfigBootstrapData['pluginsConfig']['file'] = array('json', 'plugins.json');
         $expectedConfigBootstrapData['pluginsConfig']['object'] = 'SprayFire.Config.JsonConfig';
         $expectedConfigBootstrapData['pluginsConfig']['map-key'] = 'PluginsConfig';
         $actualConfigBootstrapData = $BootstrapData->ConfigBootstrap;
@@ -59,7 +67,7 @@ class BootstrapDataTest extends \PHPUnit_Framework_TestCase {
         $expectedLoggingBootstrapData = array();
         $expectedLoggingBootstrapData['emergency'] = array();
         $expectedLoggingBootstrapData['emergency']['object'] = 'SprayFire.Logging.Logifier.SysLogLogger';
-        $expectedLoggingBootstrapData['emergency']['blueprint'] = array('SprayFire', \LOG_NDELAY, \LOG_LOCAL2);
+        $expectedLoggingBootstrapData['emergency']['blueprint'] = array('SprayFire', \LOG_NDELAY, \LOG_USER);
         $expectedLoggingBootstrapData['error'] = array();
         $expectedLoggingBootstrapData['error']['object'] = 'SprayFire.Logging.Logifier.ErrorLogLogger';
         $expectedLoggingBootstrapData['error']['blueprint'] = array();
@@ -70,11 +78,20 @@ class BootstrapDataTest extends \PHPUnit_Framework_TestCase {
         $expectedLoggingBootstrapData['info']['blueprint'] = array('sprayfire-info.txt');
         $actualLoggingBootstrapData = $BootstrapData->LoggingBootstrap;
 
-        $this->assertSame($expectedConfigBootstrapData, $actualConfigBootstrapData);
         $this->assertSame($expectedDirectoryBootstrapData, $actualDirectoryBootstrapData);
+        $this->assertSame($expectedConfigBootstrapData, $actualConfigBootstrapData);
         $this->assertSame($expectedIniBootstrapData, $actualIniBootstrapData);
         $this->assertSame($expectedLoggingBootstrapData, $actualLoggingBootstrapData);
+    }
 
+    public function testInvalidBootstrapData() {
+        $primaryConfig = \SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework/config/primary-configuration-test-no-exist.php';
+        $paths = array();
+        $BootstrapData = new \SprayFire\Bootstrap\BootstrapData($primaryConfig, $paths);
+        $this->assertSame(array(), $BootstrapData->PathGenBootstrap);
+        $this->assertSame(array(), $BootstrapData->ConfigBootstrap);
+        $this->assertSame(array(), $BootstrapData->LoggingBootstrap);
+        $this->assertSame(array(), $BootstrapData->IniBootstrap);
     }
 
 
