d4b8cb783df55023df737009b2909a7211fee094
Charles Sprayberry <cspray@gmail.com>
2012-01-12 16:22:12 -0500

    ADD BootstrapData a class to get BootstrapDataTest running with no unexpected failures

---
+++ libs/SprayFire/Bootstrap/BootstrapData.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ */
+
+namespace SprayFire\Bootstrap;
+
+/**
+ * @brief
+ */
+class BootstrapData extends \SprayFire\Config\ArrayConfig {
+
+    public function __construct($primaryConfigPath, array $directoryPaths) {
+        
+    }
+
+}
