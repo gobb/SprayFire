ca00f2ebdcd6a56c69e1b46f5d14ca7664754d10
Charles Sprayberry <cspray@gmail.com>
2012-01-14 17:35:13 -0500

    UPD LogDelegatorTest adding more assertions to properly check that appropriate logs are being used and instantiated by the LogDelegator

--- libs/SprayFire/Test/Cases/Logger/LogDelegatorTest.php
+++ libs/SprayFire/Test/Cases/Logger/LogDelegatorTest.php
@@ -26,6 +26,12 @@ class LogDelegatorTest extends \PHPUnit_Framework_TestCase {
         $LogDelegator->setDebugLogger($LogObject, $debugOptions);
         $LogDelegator->setInfoLogger($LogObject, $infoOptions);
 
+        $LogDelegator->logEmergency('This is an emergency message.');
+        $LogDelegator->logError('This is an error message.');
+        $LogDelegator->logDebug('This is a debug message.');
+        $LogDelegator->logInfo('This is an info message.');
+
+
         $ReflectedDelegator = new \ReflectionObject($LogDelegator);
         $EmergencyLogProperty = $ReflectedDelegator->getProperty('EmergencyLogger');
         $EmergencyLogProperty->setAccessible(true);
@@ -33,13 +39,47 @@ class LogDelegatorTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue($EmergencyLogger instanceof \SprayFire\Test\Helpers\TestDelegatorLogger);
         $emergencyLoggerVal = $EmergencyLogger->getOptions();
         $this->assertSame('emergency', $emergencyLoggerVal);
+        $expectedEmergencyMessages = array();
+        $expectedEmergencyMessages[0] = array();
+        $expectedEmergencyMessages[0]['message'] = 'This is an emergency message.';
+        $expectedEmergencyMessages[0]['options'] = array();
+        $this->assertSame($expectedEmergencyMessages, $EmergencyLogger->getLoggedMessages());
 
         $ErrorLogProperty = $ReflectedDelegator->getProperty('ErrorLogger');
         $ErrorLogProperty->setAccessible(true);
         $ErrorLogger = $ErrorLogProperty->getValue($LogDelegator);
         $this->assertTrue($ErrorLogger instanceof \SprayFire\Test\Helpers\TestDelegatorLogger);
         $errorLoggerVal = $ErrorLogger->getOptions();
-        $this->assertSame('', $errorLoggerVal);
+        $this->assertSame('error', $errorLoggerVal);
+        $expectedErrorMessages = array();
+        $expectedErrorMessages[0] = array();
+        $expectedErrorMessages[0]['message'] = 'This is an error message.';
+        $expectedErrorMessages[0]['options'] = array();
+        $this->assertSame($expectedErrorMessages, $ErrorLogger->getLoggedMessages());
+
+        $InfoLogProperty = $ReflectedDelegator->getProperty('InfoLogger');
+        $InfoLogProperty->setAccessible(true);
+        $InfoLogger = $InfoLogProperty->getValue($LogDelegator);
+        $this->assertTrue($InfoLogger instanceof \SprayFire\Test\Helpers\TestDelegatorLogger);
+        $infoLoggerVal = $InfoLogger->getOptions();
+        $this->assertSame('info', $infoLoggerVal);
+        $expectedInfoMessages = array();
+        $expectedInfoMessages[0] = array();
+        $expectedInfoMessages[0]['message'] = 'This is an info message.';
+        $expectedInfoMessages[0]['options'] = array();
+        $this->assertSame($expectedInfoMessages, $InfoLogger->getLoggedMessages());
+
+        $DebugLogProperty = $ReflectedDelegator->getProperty('DebugLogger');
+        $DebugLogProperty->setAccessible(true);
+        $DebugLogger = $DebugLogProperty->getValue($LogDelegator);
+        $this->assertTrue($DebugLogger instanceof \SprayFire\Test\Helpers\TestDelegatorLogger);
+        $debugLoggerVal = $DebugLogger->getOptions();
+        $this->assertSame('debug', $debugLoggerVal);
+        $expectedDebugMessages = array();
+        $expectedDebugMessages[0] = array();
+        $expectedDebugMessages[0]['message'] = 'This is a debug message.';
+        $expectedDebugMessages[0]['options'] = array();
+        $this->assertSame($expectedDebugMessages, $DebugLogger->getLoggedMessages());
     }
 
 
