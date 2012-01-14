36c4b40b1553c7ce5498af42d8411db1b82b2072
Charles Sprayberry <cspray@gmail.com>
2012-01-14 16:37:40 -0500

    ADD LogDelegatorTest adding some basic tests to make sure right objects are being set by the LogDelegator

---
+++ libs/SprayFire/Test/Cases/Logger/LogDelegatorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ */
+
+namespace SprayFire\Test\Logger;
+
+/**
+ * @brief
+ */
+class LogDelegatorTest extends \PHPUnit_Framework_TestCase {
+
+    public function testLogDelegator() {
+        $LogObject = 'SprayFire.Test.Helpers.TestDelegatorLogger';
+        $emergencyOptions = array('emergency');
+        $errorOptions = array('error');
+        $debugOptions = array('debug');
+        $infoOptions = array('info');
+
+        $Factory = new \SprayFire\Logging\Logifier\LoggerFactory();
+        $LogDelegator = new \SprayFire\Logging\Logifier\LogDelegator($Factory);
+        $LogDelegator->setEmergencyLogger($LogObject, $emergencyOptions);
+        $LogDelegator->setErrorLogger($LogObject, $errorOptions);
+        $LogDelegator->setDebugLogger($LogObject, $debugOptions);
+        $LogDelegator->setInfoLogger($LogObject, $infoOptions);
+
+        $ReflectedDelegator = new \ReflectionObject($LogDelegator);
+        $EmergencyLogProperty = $ReflectedDelegator->getProperty('EmergencyLogger');
+        $EmergencyLogger = $EmergencyLogProperty->getValue($LogDelegator);
+        $this->assertTrue($EmergencyLogger instanceof \SprayFire\Test\Helpers\TestDelegatorLogger);
+    }
+
+
+}
