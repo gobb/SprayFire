4d9596dd6ff44985343cd06e08925bd8baab5c9d
Charles Sprayberry <cspray@gmail.com>
2012-01-15 00:43:23 -0500

    ADD LoggerBootstrapTest initial test for the logger bootstrap to see if we can create the appropriate LogDelegator

---
+++ libs/SprayFire/Test/Cases/Bootstrap/LoggerBootstrapTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ */
+
+namespace SprayFire\Test\Cases\Bootstrap;
+
+/**
+ * @brief
+ */
+class LoggerBootstrapTest extends \PHPUnit_Framework_TestCase {
+
+    public function testLogOverseerCreationWithValidConfig() {
+
+        $infoLogFile = \SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework/logs/info.txt';
+        \touch($infoLogFile);
+        \chmod($infoLogFile, 0777);
+        $InfoFileObject = new \SplFileInfo($infoLogFile);
+        $config = array(
+            'emergency' => array(
+                'object' => 'SprayFire.Logging.Logifier.SysLogLogger',
+                'blueprint' => array()
+            ),
+            'error' => array(
+                'object' => 'SprayFire.Logging.Logifier.ErrorLogLogger',
+                'blueprint' => array()
+            ),
+            'info' => array(
+                'object' => 'SprayFire.Logging.Logifier.FileLogger',
+                'blueprint' => array($InfoFileObject)
+            ),
+            'debug' => array(
+                'object' => 'SprayFire.Logging.Logifier.NullLogger',
+                'blueprint' => array()
+            )
+        );
+        $ConfigObject = new \SprayFire\Config\ArrayConfig($config, false);
+        $LoggerFactory = new \SprayFire\Logging\Logifier\LoggerFactory();
+        $LoggerBootstrap = new \SprayFire\Bootstrap\LoggerBootstrap($ConfigObject, $LoggerFactory);
+        $LogDelegator = $LoggerBootstrap->runBootstrap();
+
+        $this->assertTrue($LogDelegator instanceof \SprayFire\Logging\Logifier\LogDelegator);
+
+    }
+
+
+}
