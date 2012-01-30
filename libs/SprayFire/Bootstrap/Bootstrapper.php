9073c4092267f566f6baa2e08fae45db4d312fb6
Charles Sprayberry <cspray@gmail.com>
2012-01-29 23:11:17 -0500

    UPD SprayFire\Bootstrap\Bootstrapper updating docs to show BootstrapFailedException should be thrown

--- libs/SprayFire/Bootstrap/Bootstrapper.php
+++ libs/SprayFire/Bootstrap/Bootstrapper.php
@@ -16,6 +16,15 @@ interface Bootstrapper {
     /**
      * @brief A method that should do whatever bootstrapping features are needed
      * for that particular bootstrap.
+     *
+     * @details
+     * It should be noted that if there is a problem running the bootstrap then a
+     * SprayFire.Exception.BootstrapFailedException should be thrown.  If you throw
+     * a different type of exception from this method then the calling code may
+     * not properly catch it.
+     *
+     * @return A container storing objects created by a given bootstrap
+     * @throws SprayFire.Exception.BootstrapFailedException
      */
     public function runBootstrap();
 
