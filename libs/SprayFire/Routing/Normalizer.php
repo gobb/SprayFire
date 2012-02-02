28bb317bc3316a905609e40e667f17cec4b63544
Charles Sprayberry <cspray@gmail.com>
2012-02-02 15:31:28 -0500

    DEL Util/Normalizer removing unneeded interface

---
+++ libs/SprayFire/Routing/Normalizer.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds a class that will normalize controller and action names for routing
+ * purposes.
+ */
+
+namespace SprayFire\Routing;
+
+/**
+ * @brief This class will take a controller or action URI fragment and return a
+ * string that is formatted to follow the rules defined at http://www.github.com/cspray/SprayFire/wiki/Routing.
+ */
+class Normalizer extends \SprayFire\Util\CoreObject {
+
+    public function normalizeController($controller) {
+
+
+    }
+
+    public function normalizeAction($action) {
+
+    }
+
+}
