534f5ea2a98acedf67278ff77411bda7f9d82942
Charles Sprayberry <cspray@gmail.com>
2012-02-02 15:40:25 -0500

    UPD NormalizerTest adding test for controller with underscore

--- libs/SprayFire/Test/Cases/Util/NormalizerTest.php
+++ libs/SprayFire/Test/Cases/Util/NormalizerTest.php
@@ -12,12 +12,26 @@ namespace SprayFire\Test\Cases\Util;
  */
 class NormalizerTest extends \PHPUnit_Framework_TestCase {
 
+    protected $Normalizer;
+
+    public function setUp() {
+        if (!$this->Normalizer) {
+            $this->Normalizer = new \SprayFire\Routing\Normalizer();
+        }
+    }
+
     public function testNormalizingPlainJaneController() {
         $requested = 'blog';
         $expected = 'Blog';
-        $Normalizer = new \SprayFire\Routing\Normalizer();
-        $actual = $Normalizer->normalizeController($requested);
+        $actual = $this->Normalizer->normalizeController($requested);
 
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeCompoundController() {
+        $requested = 'blog_post';
+        $expected = 'BlogPost';
+        $actual = $this->Normalizer->normalizeController($requested);
         $this->assertSame($expected, $actual);
     }
 
