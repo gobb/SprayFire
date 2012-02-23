21dcf411f94e37631be3a4356f17add45cc69f36
Charles Sprayberry <cspray@gmail.com>
2012-02-22 19:41:20 -0500

    ADD the actual ini config bootstrap and not the test for the bootstrap…derp

---
+++ libs/SprayFire/Bootstrap/IniConfigBootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds a class to set the configuration values for php.ini
+ */
+
+namespace SprayFire\Bootstrap;
+
+/**
+ * @uses SprayFire.Bootstrap.Bootstrapper
+ * @uses SprayFire.Util.CoreObject
+ */
+class IniConfigBootstrap extends \SprayFire\Util\CoreObject {
+
+
+
+}
