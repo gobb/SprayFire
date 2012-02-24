fd1d75f47be078c736d4e578c9b31a961620bb26
Charles Sprayberry <cspray@gmail.com>
2012-02-24 09:11:10 -0500

    UPD getting IniConfigBootstrapTest running

--- libs/SprayFire/Bootstrap/IniConfigBootstrap.php
+++ libs/SprayFire/Bootstrap/IniConfigBootstrap.php
@@ -11,8 +11,16 @@ namespace SprayFire\Bootstrap;
  * @uses SprayFire.Bootstrap.Bootstrapper
  * @uses SprayFire.Util.CoreObject
  */
-class IniConfigBootstrap extends \SprayFire\Util\CoreObject {
+class IniConfigBootstrap extends \SprayFire\Util\CoreObject implements \SprayFire\Bootstrap\Bootstrapper {
 
+    /**
+     * @brief Will set the global ini configuration settings and either the development
+     * or production ini configuration.
+     *
+     * @return void
+     */
+    public function runBootstrap() {
 
+    }
 
 }
