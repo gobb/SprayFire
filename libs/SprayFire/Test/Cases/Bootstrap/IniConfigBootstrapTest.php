fb9c69b4586261b25eaa2651a490a0b019111e76
Charles Sprayberry <cspray@gmail.com>
2012-04-21 00:55:59 -0400

    UPD finalizing test for IniConfigBootstrap

--- libs/SprayFire/Test/Cases/Bootstrap/IniConfigBootstrapTest.php
+++ libs/SprayFire/Test/Cases/Bootstrap/IniConfigBootstrapTest.php
@@ -40,24 +40,44 @@ class IniConfigBootstrapTest extends \PHPUnit_Framework_TestCase {
         $this->defaultGlobalIniSettings['expose_php'] = $exposePhp;
     }
 
-    public function testIniConfigBootstrapGlobalIniSettingsOnly() {
+    public function testIniConfigBootstrap() {
         $arrayConfig = array();
-        $arrayConfig['developmentMode'] = false;
-        $arrayConfig['globalIniSettings'] = array();
-        $arrayConfig['globalIniSettings']['allow_url_fopen'] = 1;
-        $arrayConfig['globalIniSettings']['allow_url_include'] = 1;
-        $arrayConfig['globalIniSettings']['asp_tags'] = 0;
-        $arrayConfig['globalIniSettings']['date.timezone'] = 'America/Los_Angeles';
-        $arrayConfig['globalIniSettings']['default_charset'] = '';
-        $arrayConfig['globalIniSettings']['default_mimetype'] = 'text/plain';
-        $arrayConfig['globalIniSettings']['assert.active'] = 1;
+        $arrayConfig['allow_url_fopen'] = 1;
+        $arrayConfig['allow_url_include'] = 1;
+        $arrayConfig['asp_tags'] = 0;
+        $arrayConfig['date.timezone'] = 'America/Los_Angeles';
+        $arrayConfig['default_charset'] = 'UTF-8';
+        $arrayConfig['default_mimetype'] = 'text/plain';
+        $arrayConfig['assert.active'] = 1;
         $arrayConfig['magic_quotes_gpc'] = 0;
         $arrayConfig['magic_quotes_runtime'] = 0;
         $arrayConfig['expose_php'] = 1;
-        $Config = new \SprayFire\Config\ArrayConfig($arrayConfig);
-        $Bootstrap = new \SprayFire\Bootstrap\IniConfigBootstrap($Config);
-        $trueOrFalse = $Bootstrap->runBootstrap();
-        $this->assertTrue($trueOrFalse);
+
+        $Bootstrap = new \SprayFire\Bootstrap\IniConfigBootstrap($arrayConfig);
+        $Bootstrap->runBootstrap();
+
+        $allowUrlFopen = \ini_get('allow_url_fopen');
+        $allowUrlInclude = \ini_get('allow_url_include');
+        $aspTags = \ini_get('asp_tags');
+        $dateTimezone = \ini_get('date.timezone');
+        $defaultCharset = \ini_get('default_charset');
+        $defaultMimetype = \ini_get('default_mimetype');
+        $assertActive = \ini_get('assert.active');
+        $magicQuotesGpc = \ini_get('magic_quotes_gpc');
+        $magicQuotesRuntime = \ini_get('magic_quotes_runtime');
+        $exposePhp = \ini_get('expose_php');
+
+        $this->assertSame(1, $allowUrlFopen, 'allow_url_fopen was not set properly');
+        $this->assertSame(1, $allowUrlInclude, 'allow_url_include was not set properly');
+        $this->assertSame(0, $aspTags, 'asp_tags was not set properly');
+        $this->assertSame('America/Los_Angeles', $dateTimezone, 'date.timezone was not set properly');
+        $this->assertSame('UTF-8', $defaultCharset, 'default_charset was not set properly');
+        $this->assertSame('text/plain', $defaultMimetype, 'default_mimetype was not set properly');
+        $this->assertSame(1, $assertActive, 'assert.active was not set properly');
+        $this->assertSame(0, $magicQuotesGpc, 'magic_quotes_gpc was not set properly');
+        $this->assertSame(0, $magicQuotesRuntime, 'magic_quotes_runtime was not set properly');
+        $this->assertSame(1, $exposePhp, 'expose_php was not set properly');
+
     }
 
     public function tearDown() {
