2a8f7a56b1f7a83b2562735ba47a0cb3af26a6f2
Charles Sprayberry <cspray@gmail.com>
2012-04-22 00:03:47 -0400

    UPD adding test for PrimaryBootstrap

---
+++ libs/SprayFire/Test/Cases/Bootstrap/PrimaryBootstrapTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SprayFire\Test\Cases\Bootstrap;
+
+/**
+ *
+ */
+class PrimaryBootstrapTest extends \PHPUnit_Framework_TestCase {
+
+    public function testRunningBootstrap() {
+        $configFile = \SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework/config/primary-configuration-test.php';
+        $this->assertFileExists($configFile);
+        $installPath = \SPRAYFIRE_ROOT;
+        $libsPath = $installPath . '/libs';
+        $configPath = $installPath . '/config';
+        $appPath = $installPath . '/app';
+        $logsPath = $installPath . '/logs';
+        $webPath = $installPath . '/web';
+        $paths = \compact('installPath', 'libsPath', 'configPath', 'appPath', 'logsPath', 'webPath');
+        $Data = new \SprayFire\Bootstrap\BootstrapData($configFile, $paths);
+        $Bootstrap = new \SprayFire\Bootstrap\PrimaryBootstrap($Data);
+    }
+
+
+
+}
+
+// End PrimaryBootstrapTest
