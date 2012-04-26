e3b3a6ea8af8305929158ae0d4c1e28e6c0da349
Charles Sprayberry <cspray@gmail.com>
2012-04-26 19:03:39 -0400

    UPD adding test for a blank requested URI

--- libs/SprayFire/Test/Cases/Routing/Routifier/RequestUriTest.php
+++ libs/SprayFire/Test/Cases/Routing/Routifier/RequestUriTest.php
@@ -28,6 +28,21 @@ class RequestUriTest extends \PHPUnit_Framework_TestCase {
         $this->assertSame($uri, $requestedUri);
     }
 
+    public function testEmptyRequestedUri() {
+        $uri = '';
+        $root = 'sprayfire';
+        $RequestUri = new \SprayFire\Routing\Routifier\RequestUri($uri, $root);
+        $controller = $RequestUri->getControllerFragment();
+        $action = $RequestUri->getActionFragment();
+        $parameters = $RequestUri->getParameters();
+        $requestedUri = $RequestUri->getRequestedUri();
+
+        $this->assertSame(\SprayFire\Routing\Uri::NO_CONTROLLER_REQUESTED, $controller);
+        $this->assertSame(\SprayFire\Routing\Uri::NO_ACTION_REQUESTED, $action);
+        $this->assertSame(array(), $parameters);
+        $this->assertSame($uri, $requestedUri);
+    }
+
     public function tearDown() {
 
     }
