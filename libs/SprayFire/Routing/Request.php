1f0c6d7a31f0ba7ad3824d24b86e65cce3c07aaa
Charles Sprayberry <cspray@gmail.com>
2012-05-29 20:01:45 -0400

    UPD getter methods for info this class provides

--- libs/SprayFire/Routing/Request.php
+++ libs/SprayFire/Routing/Request.php
@@ -2,14 +2,44 @@
 
 /**
  * @file
- * @brief
+ * @brief Interface for classes that answer when the Dispatch layer asks for info
+ * needed to complete a request.
  */
 
 namespace SprayFire\Routing;
 
 /**
- * @brief 
+ * @brief
  */
 interface Request {
+
+    /**
+     * @brief Return the Uri instance that was created to determine this request
+     *
+     * @return SprayFire.Routing.Uri
+     */
+    public function getUri();
+
+    /**
+     * @brief Return an array of strings that represent the top-level namespace
+     * for the apps that should be
+     *
+     * @return array<string>
+     */
+    public function getApps();
+
+    /**
+     * @brief Return the name of the controller to instantiate for the request
+     *
+     * @return string
+     */
+    public function getNormalizedController();
+
+    /**
+     * @brief Return the method that should be invoked on the instantiated controller.
+     *
+     * @return string
+     */
+    public function getNormalizedAction();
 
 }
