81081aed7d13f270422e2fc8b6dde71a3dea3939
Charles Sprayberry <cspray@gmail.com>
2012-05-31 22:45:47 -0400

    UPD removing old tests as they are all invalid

--- libs/SprayFire/Test/Cases/Bootstrap/Strapifier/PrimaryTest.php
+++ libs/SprayFire/Test/Cases/Bootstrap/Strapifier/PrimaryTest.php
@@ -8,25 +8,7 @@ namespace SprayFire\Test\Cases\Bootstrap\Strapifier;
 class PrimaryTest extends \PHPUnit_Framework_TestCase {
 
     public function testRunningBootstrap() {
-        $configFile = \SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework/config/primary-configuration-test.php';
-        $this->assertFileExists($configFile);
-        $installPath = \SPRAYFIRE_ROOT;
-        $libsPath = $installPath . '/libs';
-        $configPath = $installPath . '/config';
-        $appPath = $installPath . '/app';
-        $logsPath = $installPath . '/logs';
-        $webPath = $installPath . '/web';
-        $paths = \compact('installPath', 'libsPath', 'configPath', 'appPath', 'logsPath', 'webPath');
-        $Data = new \SprayFire\Bootstrap\Strapifier\Configuration($configFile, $paths);
-        $Bootstrap = new \SprayFire\Bootstrap\Strapifier\Primary($Data);
-        $SprayFireContainer = $Bootstrap->runBootstrap();
-        $this->assertInstanceOf('\\SprayFire\\Structure\\Map\\GenericMap', $SprayFireContainer, 'The SprayFireContainer is not a GenericMap');
-        $this->assertTrue($SprayFireContainer->containsKey('PathGenerator'), 'The SprayFireContainer does not contain a PathGenerator');
-        $PathGenerator = $SprayFireContainer->getObject('PathGenerator');
-        $this->assertInstanceOf('\\SprayFire\\Util\\Directory', $PathGenerator);
-        $this->assertTrue($SprayFireContainer->containsKey('LogOverseer'), 'The SprayFireContainer does not contain a LogOverseer');
-        $LogOverseer = $SprayFireContainer->getObject('LogOverseer');
-        $this->assertInstanceOf('\\SprayFire\\Logging\\Logifier\\LogDelegator', $LogOverseer);
+        
     }
 
 
