4ad5952eb9d5aa5ce1f878a86f06994ab320468c
Charles Sprayberry <cspray@gmail.com>
2012-06-02 10:44:24 -0400

    UPD removing date.timezone ini setting check

--- libs/SprayFire/Test/Cases/Bootstrap/Strapifier/IniSettingTest.php
+++ libs/SprayFire/Test/Cases/Bootstrap/Strapifier/IniSettingTest.php
@@ -17,13 +17,11 @@ class IniConfigTest extends \PHPUnit_Framework_TestCase {
     }
 
     protected function setGlobalIniSettings() {
-        $dateTimezone = \ini_get('date.timezone');
         $defaultCharset = \ini_get('default_charset');
         $defaultMimetype = \ini_get('default_mimetype');
         $assertActive = \ini_get('assert.active');
         $exposePhp = \ini_get('expose_php');
 
-        $this->defaultGlobalIniSettings['date.timezone'] = $dateTimezone;
         $this->defaultGlobalIniSettings['default_charset'] = $defaultCharset;
         $this->defaultGlobalIniSettings['default_mimetype'] = $defaultMimetype;
         $this->defaultGlobalIniSettings['assert.active'] = $assertActive;
@@ -32,7 +30,6 @@ class IniConfigTest extends \PHPUnit_Framework_TestCase {
 
     public function testIniConfigBootstrap() {
         $arrayConfig = array();
-        $arrayConfig['date.timezone'] = 'America/Los_Angeles';
         $arrayConfig['default_charset'] = 'UTF-8';
         $arrayConfig['default_mimetype'] = 'text/plain';
         $arrayConfig['assert.active'] = 1;
@@ -47,7 +44,6 @@ class IniConfigTest extends \PHPUnit_Framework_TestCase {
         $assertActive = \ini_get('assert.active');
         $exposePhp = \ini_get('expose_php');
 
-        $this->assertSame('America/Los_Angeles', $dateTimezone, 'date.timezone was not set properly');
         $this->assertSame('UTF-8', $defaultCharset, 'default_charset was not set properly');
         $this->assertSame('text/plain', $defaultMimetype, 'default_mimetype was not set properly');
         $this->assertSame('1', $assertActive, 'assert.active was not set properly');
