b3b176634886336de211a492d0a484c88d92bff9
Charles Sprayberry <cspray@gmail.com>
2012-06-02 09:59:15 -0400

    UPD adding docs and renaming first test

--- libs/SprayFire/Test/Cases/Bootstrap/Strapifier/PrimaryTest.php
+++ libs/SprayFire/Test/Cases/Bootstrap/Strapifier/PrimaryTest.php
@@ -7,12 +7,13 @@ namespace SprayFire\Test\Cases\Bootstrap\Strapifier;
  */
 class PrimaryTest extends \PHPUnit_Framework_TestCase {
 
-    public function testRunningBootstrap() {
+    /**
+     * @brief Testing that we can create a dynamic set of objects, with classes
+     * that can be loaded, and that the appropriate objects are returned in a
+     * GenericMap structure.
+     */
+    public function testGeneratingValidDynamicObjects() {
         
     }
 
-
-
 }
-
-// End PrimaryBootstrapTest
