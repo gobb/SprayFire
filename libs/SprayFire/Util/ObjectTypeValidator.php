0ab83c0dccfc810b90160c4cfc72ade210162303
Charles Sprayberry <cspray@gmail.com>
2012-06-08 12:33:17 -0400

    UPD getting rid of reflection, removing unneeded method

--- libs/SprayFire/Util/ObjectTypeValidator.php
+++ libs/SprayFire/Util/ObjectTypeValidator.php
@@ -34,49 +34,9 @@ class ObjectTypeValidator {
      * @throws InvalidArgumentException
      */
     public function throwExceptionIfObjectNotParentType($Object) {
-        if (!$this->isObjectParentType($Object)) {
+        if (!\is_a($Object)) {
             throw new \InvalidArgumentException('The value being set does not properly implement the parent type for this store.');
         }
-    }
-
-    /**
-     * @brief Determines whether or not the passed \a $Object \a implements
-     * the parent type set by the constructor dependency.
-     *
-     * @details
-     * The objet is considered valid if any of the following are true:
-     *
-     * 1) Implements the interface passed in the constructor
-     * 2) Is an instance of the class passed in the constructor
-     * 3) Is a subclass of the class passed in the constructor
-     *
-     * @param $Object SprayFire.Core.Object
-     * @return boolean
-     */
-    protected function isObjectParentType($Object) {
-        $isValid = false;
-        $parentName = $this->ReflectedParentType->getName();
-        try {
-            $ReflectedObject = new \ReflectionClass($Object);
-            if ($this->ReflectedParentType->isInterface()) {
-                if ($ReflectedObject->implementsInterface($parentName)) {
-                    $isValid = true;
-                }
-            } else {
-                if ($ReflectedObject->getName() === $parentName || $ReflectedObject->isSubclassOf($parentName)) {
-                    $isValid = true;
-                }
-            }
-        } catch (\ReflectionException $ReflectionExc) {
-            // @codeCoverageIgnoreStart
-            // The possibility of this being thrown should be very, very slim as
-            // a properly instantiated object must be passed, and thus must be
-            // available for reflection...this is a fail safe to not leak an
-            // unnecessary exception
-            error_log($ReflectionExc->getMessage());
-            // @codeCoverageIgnoreEnd
-        }
-        return $isValid;
     }
 
 }
