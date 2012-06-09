af4f560a2a802bf3cdb9f9799ff2314981a08a64
Charles Sprayberry <cspray@gmail.com>
2012-06-09 15:12:09 -0400

    ADD tests running for implementation of Service\Container

---
+++ libs/SprayFire/Service/FireBox/Container.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ */
+
+namespace SprayFire\Service\FireBox;
+
+/**
+ * @brief
+ */
+class Container extends \SprayFire\Util\CoreObject implements \SprayFire\Service\Container {
+
+
+
+    public function addService($serviceName, $callableParameters = null) {
+
+    }
+
+    public function doesServiceExist($serviceName) {
+
+    }
+
+    public function getService($serviceName) {
+
+    }
+
+    public function setServiceFactory($serviceType, \SprayFire\Factory\Factory $Factory) {
+
+    }
+    
+}
