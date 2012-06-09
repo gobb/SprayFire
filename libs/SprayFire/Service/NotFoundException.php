344a71c7d562c29e6531061419ba41a4981082f7
Charles Sprayberry <cspray@gmail.com>
2012-06-09 15:05:10 -0400

    ADD to provide exception for Service\Container interface

---
+++ libs/SprayFire/Service/NotFoundException.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * @brief Holds an exception thrown if a service requested from SprayFire.Service.Container
+ * does not exist.
+ */
+
+namespace SprayFire\Service;
+
+/**
+ * @brief
+ */
+class NotFoundException extends \Exception {
+
+
+}
