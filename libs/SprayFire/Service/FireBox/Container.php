073512556a76f1f18ce8269465d98cf837061ebb
Charles Sprayberry <cspray@gmail.com>
2012-06-11 22:14:51 -0400

    UPD adding docs, adding check to for non-callable parameters

--- libs/SprayFire/Service/FireBox/Container.php
+++ libs/SprayFire/Service/FireBox/Container.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * A class that holds a container responsible for creating and storing services.
  *
  * @author Charles Sprayberry
  */
@@ -16,19 +17,42 @@ class Container extends \SprayFire\Util\UtilObject implements \SprayFire\Service
      */
     protected $addedServices = array();
 
+    /**
+     *
+     * @param $serviceName string Namespaced name of the class representing the service
+     * @param $callableParameters callable An anonymous function returning array of service dependencies
+     * @throws InvalidArgumentException
+     */
     public function addService($serviceName, $callableParameters = null) {
+        if (!\is_callable($callableParameters)) {
+            throw new \SprayFire\Service\NotFoundException('Attempt to pass a non-callable type to a callable require parameter.');
+        }
         $serviceName = $this->convertJavaClassToPhpClass($serviceName);
         $this->addedServices[$serviceName] = $callableParameters;
     }
 
+    /**
+     * @param $serviceName string Namespaced name of the class representing the service
+     * @return boolean
+     */
     public function doesServiceExist($serviceName) {
         $serviceName = $this->convertJavaClassToPhpClass($serviceName);
         return \array_key_exists($serviceName, $this->addedServices);
     }
 
+    /**
+     * @param $serviceName string Namespaced name of the class representing the service
+     * @return Object Of type \a $serviceName
+     * @throws SprayFire.Service.NotFoundException
+     */
     public function getService($serviceName) {
+
     }
 
+    /**
+     * @param $serviceType string Namespaced name of the class representing the service
+     * @param $Factory SprayFire.Factory.Factory
+     */
     public function setServiceFactory($serviceType, \SprayFire\Factory\Factory $Factory) {
 
     }
