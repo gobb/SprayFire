4e02eb397a16513763061d34d2dcc31ecded74bd
Charles Sprayberry <cspray@gmail.com>
2012-06-21 19:07:54 -0400

    ADD initial interface for a Controller

---
+++ libs/SprayFire/Controller/Controller.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * An interface implemented by all SprayFire and app controllers; allows for the
+ * retrieval of information about the services the controller needs and provides
+ * a means to give data to the Responder
+ *
+ * @author Charles Sprayberry
+ */
+
+namespace SprayFire\Controller;
+
+interface Controller {
+
+    /**
+     * Provides the fully namespaced name of the class to use as the Responder
+     * for this controller; can be a Java or PHP-style name.
+     *
+     * @return string
+     */
+    public function getResponderName();
+
+    /**
+     * Provides an array of strings representing the namespaced name of the Models
+     * that should be available to this controller.
+     *
+     * @return array
+     */
+    public function getModels();
+
+    /**
+     * Should provide a means to give a Responder data that is to be considered
+     * unsanitized.
+     *
+     * @param $data array
+     */
+    public function giveDirtyData(array $data);
+
+    /**
+     * Provide a means to give a Responder data that is to be considered safe
+     * and should not go through sanitization.
+     *
+     * @param $data array
+     */
+    public function giveCleanData(array $data);
+
+    /**
+     * Should return an array of data given to the Responder that is considered
+     * dirty and should be sanitized in some way.
+     *
+     * @return array
+     */
+    public function getDirtyData();
+
+    /**
+     * Should return an array of data given to the Responder that is considered
+     * clean and should not be sanitized.
+     *
+     * @return array
+     */
+    public function getCleanData();
+
+}
