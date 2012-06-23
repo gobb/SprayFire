bb29875b0dd111ee343acd2a8cde687c8803e413
Charles Sprayberry <cspray@gmail.com>
2012-06-23 11:38:56 -0400

    DEL & ADD renaming and fixing tests for \FileSys\Paths

--- libs/SprayFire/Test/Cases/FileSys/PathsTest.php
+++ libs/SprayFire/Test/Cases/FileSys/PathsTest.php
@@ -2,12 +2,12 @@
 
 namespace SprayFire\Test\Cases\Util;
 
-class DirectoryTest extends \PHPUnit_Framework_TestCase {
+class PathsTest extends \PHPUnit_Framework_TestCase {
 
     public function testPathWithSubDirectoriesAsParameters() {
         $install = \SPRAYFIRE_ROOT . '/tests/mockframework';
         $paths = \compact('install');
-        $Directory = new \SprayFire\Util\Directory($paths);
+        $Directory = new \SprayFire\FileSys\Paths($paths);
         $expected = $install . '/some/path/to/the/file.php';
         $actual = $Directory->getInstallPath('some', 'path', 'to', 'the', 'file.php');
         $this->assertSame($expected, $actual);
@@ -16,7 +16,7 @@ class DirectoryTest extends \PHPUnit_Framework_TestCase {
     public function testPathWithSubDirectoriesAsArray() {
         $install = \SPRAYFIRE_ROOT . '/tests/mockframework';
         $paths = \compact('install');
-        $Directory = new \SprayFire\Util\Directory($paths);
+        $Directory = new \SprayFire\FileSys\Paths($paths);
         $expected = $install . '/libs/SprayFire/checking/this/directory';
         $actual = $Directory->getInstallPath(array('libs', 'SprayFire', 'checking', 'this', 'directory'));
         $this->assertSame($expected, $actual);
@@ -26,7 +26,7 @@ class DirectoryTest extends \PHPUnit_Framework_TestCase {
         $install = \SPRAYFIRE_ROOT . '/tests/mockframework';
         $web = $install . '/web';
         $paths = \compact('install', 'web');
-        $Directory = new \SprayFire\Util\Directory($paths);
+        $Directory = new \SprayFire\FileSys\Paths($paths);
         $expected = '/mockframework/web/css/main.css';
         $actual = $Directory->getUrlPath('css', 'main.css');
         $this->assertSame($expected, $actual);
