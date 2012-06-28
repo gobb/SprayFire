45fb45976c2f7daf44209af9977f48286c2f1de2
Charles Sprayberry <cspray@gmail.com>
2012-06-28 18:35:18 -0400

    UPD initial commit, not passing all tests

---
+++ libs/SprayFire/Http/ResourceIdentifier.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * The framework's base implementation of the SprayFire.Http.Uri interface.
+ *
+ * @author Charles Sprayberry
+ */
+
+namespace SprayFire\Http;
+
+class ResourceIdentifier extends \SprayFire\CoreObject implements \SprayFire\Http\Uri {
+
+    /**
+     * @property string
+     */
+    protected $authority = '';
+
+    /**
+     * @property string
+     */
+    protected $path = '';
+
+    /**
+     * @property int
+     */
+    protected $port = 80;
+
+    /**
+     * @property string
+     */
+    protected $query = '';
+
+    /**
+     * @property string
+     */
+    protected $scheme = '';
+
+    /**
+     * If $_server is null then the $_SERVER superglobal will be used to parse the
+     * URI fragments.
+     *
+     * @param array $_server
+     */
+    public function __construct(array $_server = null) {
+        if (empty($_server)) {
+            $_server = $_SERVER;
+        }
+        $this->parseParameters($_server);
+    }
+
+    /**
+     * After calling this method all class properties will be properly set to the
+     * values, if present, in $_server.
+     *
+     * @param array $_server
+     */
+    protected function parseParameters(array $_server) {
+        if (isset($_server['REMOTE_PORT'])) {
+            $this->port = $_server['REMOTE_PORT'];
+        }
+
+        if (isset($_server['HTTP_HOST'])) {
+            $this->authority = $_server['HTTP_HOST'] . ':' . $this->port;
+        }
+
+        if (isset($_server['QUERY_STRING'])) {
+            $this->query = $_server['QUERY_STRING'];
+        }
+
+        if (isset($_server['HTTPS']) && \filter_var($_server['HTTPS'], \FILTER_VALIDATE_BOOLEAN)) {
+            $this->scheme = 'https';
+        } else {
+            $this->scheme = 'http';
+        }
+
+        if (isset($_server['REQUEST_URI']) && !empty($_server['REQUEST_URI'])) {
+            $pathLength = \strpos($_server['REQUEST_URI'], '?');
+            if ($pathLength !== false) {
+                $this->path = \substr($_server['REQUEST_URI'], 0, $pathLength);
+            } else {
+                $this->path = $_server['REQUEST_URI'];
+            }
+        }
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthority() {
+        return $this->authority;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath() {
+        return $this->path;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPort() {
+        return $this->port;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery() {
+        return $this->query;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScheme() {
+        return $this->scheme;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString() {
+        return $this->scheme . '://' . $this->authority . $this->path . $this->query;
+    }
+
+}
