86d39dd5fad0cb9d60fe246cb1fd0f933e044c73
Charles Sprayberry <cspray@gmail.com>
2012-07-04 10:56:49 -0400

    ADD bringing Normalizer under correct namespace

---
+++ libs/SprayFire/Http/Routing/Normalizer.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Will normalize controller and action names for routing purposes.
+ *
+ * @author Charles Sprayberry
+ * @license Governed by the LICENSE file found in the root directory of this source
+ * code
+ */
+
+namespace SprayFire\Http\Routing;
+
+/**
+ * Will take a controller or action URI fragment and return a string that is formatted
+ * to follow the rules defined at http://www.github.com/cspray/SprayFire/wiki/Routing.
+ */
+class Normalizer extends \SprayFire\CoreObject {
+
+    /**
+     * PCRE regex pattern used to remove all non-alphabetic/space characters
+     *
+     * @property string
+     */
+    protected $characterRegex = '/[^A-Za-z\s]/';
+
+    /**
+     * @param string $controller
+     * @return string
+     */
+    public function normalizeController($controller) {
+        $class = \strtolower($controller);
+        $class = $this->replaceUnderscoresWithSpaces($class);
+        $class = $this->replaceDashesWithSpaces($class);
+        $class = $this->removeInvalidCharacters($class);
+        return $this->makePascalCased($class);
+    }
+
+    /**
+     * @param strubg $action
+     * @return string
+     */
+    public function normalizeAction($action) {
+        $action = \strtolower($action);
+        $action = $this->replaceUnderscoresWithSpaces($action);
+        $action = $this->replaceDashesWithSpaces($action);
+        $action = $this->removeInvalidCharacters($action);
+        return $this->makeCamelCased($action);
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function replaceUnderscoresWithSpaces($string) {
+        $underscore = '_';
+        return $this->replaceCharacterWithSpaces($string, $underscore);
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function replaceDashesWithSpaces($string) {
+        $dash = '-';
+        return $this->replaceCharacterWithSpaces($string, $dash);
+    }
+
+    /**
+     * @param strubg $string
+     * @param string $character
+     * @return string
+     */
+    protected function replaceCharacterWithSpaces($string, $character) {
+        $space = ' ';
+        if (\strpos($string, $character) === false) {
+            return $string;
+        }
+        return (string) \str_replace($character, $space, $string);
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function removeInvalidCharacters($string) {
+        return (string) \preg_replace($this->characterRegex, '', $string);
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function makePascalCased($string) {
+        $class = \ucwords($string);
+        $class = \str_replace(' ', '', $class);
+        return (string) $class;
+    }
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    protected function makeCamelCased($string) {
+        $string = $this->makePascalCased($string);
+        return \lcfirst($string);
+    }
+
+
+
+}
---
+++ libs/SprayFire/Test/Cases/Http/Routing/NormalizerTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @file
+ * @brief
+ */
+
+namespace SprayFire\Test\Cases\Http\Routing;
+
+/**
+ * @brief
+ */
+class NormalizerTest extends \PHPUnit_Framework_TestCase {
+
+    protected $Normalizer;
+
+    public function setUp() {
+        $this->Normalizer = new \SprayFire\Http\Routing\Normalizer();
+    }
+
+    public function testNormalizingPlainJaneController() {
+        $requested = 'blog';
+        $expected = 'Blog';
+        $actual = $this->Normalizer->normalizeController($requested);
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeCompoundControllerWithUnderscores() {
+        $requested = 'blog_post';
+        $expected = 'BlogPost';
+        $actual = $this->Normalizer->normalizeController($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeCompoundControllerWithDashes() {
+        $requested = 'another-blog-post';
+        $expected = 'AnotherBlogPost';
+        $actual = $this->Normalizer->normalizeController($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeControllerWithInvalidCharacters() {
+        $requested = 'something*with#invalid_-characters%^';
+        $expected = 'SomethingwithinvalidCharacters';
+        $actual = $this->Normalizer->normalizeController($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizePlainJaneAction() {
+        $requested = 'action';
+        $expected = 'action';
+        $actual = $this->Normalizer->normalizeAction($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeActionWithUnderscores() {
+        $requested = 'action_perform';
+        $expected = 'actionPerform';
+        $actual = $this->Normalizer->normalizeAction($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeActionWithDashes() {
+        $requested = 'the-action-requested-was-this-one';
+        $expected = 'theActionRequestedWasThisOne';
+        $actual = $this->Normalizer->normalizeAction($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testNormalizeActionWithInvalidCharacters() {
+        $requested = 'this-action*hith#invalid_stuff            ----_%^';
+        $expected = 'thisActionhithinvalidStuff';
+        $actual = $this->Normalizer->normalizeAction($requested);
+        $this->assertSame($expected, $actual);
+    }
+
+}
