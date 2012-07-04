e144b1cc6b1cf0d82bc78a5e3525c3da83c1b3d4
Charles Sprayberry <cspray@gmail.com>
2012-07-04 14:37:54 -0400

    UPD adding some more checks to test, getting services needed created

--- libs/SprayFire/Test/Cases/Http/Routing/StandardRouterTest.php
+++ libs/SprayFire/Test/Cases/Http/Routing/StandardRouterTest.php
@@ -15,10 +15,16 @@ class StandardRouterTest extends \PHPUnit_Framework_TestCase {
         $Uri = new \SprayFire\Http\ResourceIdentifier($_server);
         $Headers = new \SprayFire\Http\StandardRequestHeaders($_server);
         $Request = new \SprayFire\Http\StandardRequest($Uri, $Headers);
-        $Router = new \SprayFire\Http\Routing\StandardRouter();
+
+        $Normalizer = new \SprayFire\Http\Routing\Normalizer();
+        $configPath = \SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework/config/SprayFire/routes.json';
+        $Router = new \SprayFire\Http\Routing\StandardRouter($Normalizer, $configPath, 'SprayFire');
 
         $RoutedRequest = $Router->getRoutedRequest($Request);
         $this->assertInstanceOf('\\SprayFire\\Http\\Routing\\RoutedRequest', $RoutedRequest);
+        $this->assertSame('SprayTest.Controller.Controller', $RoutedRequest->getController());
+        $this->assertSame('action', $RoutedRequest->getAction());
+        $this->assertSame(array('param1', 'param2', 'param3'), $RoutedRequest->getParameters());
     }
 
 }
