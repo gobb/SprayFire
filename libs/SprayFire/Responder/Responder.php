52ae7b2027b94ea2d4b7bd3e75b331d348abbe40
Charles Sprayberry <cspray@gmail.com>
2012-07-08 15:50:10 -0400

    UPD fixing some doc errors

--- libs/SprayFire/Responder/Responder.php
+++ libs/SprayFire/Responder/Responder.php
@@ -1,8 +1,12 @@
 <?php
 
 /**
+ * An interface responsible for generating and sending a response based on a given
+ * controller.
  *
  * @author Charles Sprayberry
+ * @license Governed by the LICENSE file found in the root directory of this source
+ * code
  */
 
 namespace SprayFire\Responder;
@@ -13,7 +17,7 @@ interface Responder {
      * Should return an array of snitized data based on the type of responder
      * being used.
      *
-     * @param $data array
+     * @param array $data
      * @return array
      */
     public function sanitizeData(array $data);
@@ -22,7 +26,7 @@ interface Responder {
      * Should sanitize the appropriate data and generate a response based on the
      * $Controller data provided.
      *
-     * @param $Controller SprayFire.Controller.Controller
+     * @param SprayFire.Controller.Controller $Controller
      * @return string
      */
     public function generateResponse(\SprayFire\Controller\Controller $Controller);
