c478d14edd7bb77cb3b8040972e5d18a44b78931
Charles Sprayberry <cspray@gmail.com>
2012-07-14 15:53:56 -0400

    UPD adding default controller, moving launch front page to this controller

---
+++ libs/SprayFire/Controller/Pages.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *
+ * @author Charles Sprayberry
+ * @license Governed by the LICENSE file found in the root directory of this source
+ * code
+ */
+
+namespace SprayFire\Controller;
+
+use \SprayFire\Controller\Base as BaseController;
+
+class Pages extends BaseController {
+
+    public function index() {
+        $this->templatePath = $this->service('Paths')->getLibsPath('SprayFire', 'Responder', 'html', 'debug-content.php');
+        $this->layoutPath = $this->service('Paths')->getLibsPath('SprayFire', 'Responder', 'html', 'layout', 'default.php');
+
+        $message = 'The requested resource could not be found.';
+        $styleCss = $this->service('Paths')->getUrlPath('css', 'sprayfire.style.css');
+        $sprayFireLogo = $this->service('Paths')->getUrlPath('images', 'sprayfire-logo-bar-75.png');
+        $serverData = \print_r($_SERVER, true);
+        $sessionData = \print_r($_SESSION, true);
+        $postData = \print_r($_POST, true);
+        $getData = \print_r($_GET, true);
+        $controller = \get_class();
+
+        $data = \compact(
+                    'message',
+                    'styleCss',
+                    'sprayFireLogo',
+                    'serverData',
+                    'sessionData',
+                    'postData',
+                    'getData',
+                    'controller'
+                );
+        $this->giveDirtyData($data);
+    }
+
+}
