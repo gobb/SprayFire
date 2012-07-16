039605b2f9c42b89a6ed68c3491bb5359a773cd2
Charles Sprayberry <cspray@gmail.com>
2012-07-16 08:39:37 -0400

    UPD adding action and parameters to debug content

--- libs/SprayFire/Responder/html/debug-content.php
+++ libs/SprayFire/Responder/html/debug-content.php
@@ -6,5 +6,9 @@
 <pre><?php echo $postData; ?></pre>
 <h2>GET Data</h2>
 <pre><?php echo $getData; ?></pre>
-<h2>Controller Invoked</h2>
+<h2>Controller Instantiated</h2>
 <pre><?php echo $controller; ?></pre>
+<h2>Controller Action invoked</h2>
+<pre><?php echo $action; ?></pre>
+<h2>Parameters</h2>
+<pre><?php echo $parameters; ?></pre>
