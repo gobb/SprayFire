1ddc74f83fef8b87391e83ae4a813c61fbce83ff
Charles Sprayberry <cspray@gmail.com>
2012-07-19 22:05:00 -0400

    UPD adding some more tests, dependencies for dispatcher

--- libs/SprayFire/Test/Cases/Dispatcher/FireDispatcherTest.php
+++ libs/SprayFire/Test/Cases/Dispatcher/FireDispatcherTest.php
@@ -17,6 +17,11 @@ class FireDispatcherTest extends \PHPUnit_Framework_TestCase {
 
     protected $Logger;
 
+    /**
+     * @property SprayFire.Test.Helpers.DevelopmentLogger
+     */
+    protected $ErrorLog;
+
     public function setUp() {
         $this->Cache = new \Artax\ReflectionCacher();
         $this->Container = new \SprayFire\Service\FireBox\Container($this->Cache);
@@ -24,15 +29,17 @@ class FireDispatcherTest extends \PHPUnit_Framework_TestCase {
             $RootPaths = new \SprayFire\FileSys\RootPaths(\SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework');
             return array($RootPaths);
         });
-        $Error = $Emergency = $Debug = $Info = new \SprayFire\Test\Helpers\DevelopmentLogger();
+        $Emergency = $Debug = $Info = new \SprayFire\Test\Helpers\DevelopmentLogger();
+        $this->ErrorLog = $Error = new \SprayFire\Test\Helpers\DevelopmentLogger();
         $this->Logger = new \SprayFire\Logging\Logifier\LogDelegator($Emergency, $Error, $Debug, $Info);
     }
 
     public function testFireDispatcherTest() {
         $Router = $this->getRouter('');
+        $Logger = $this->Logger;
         $ControllerFactory = $this->getControllerFactory();
         $ResponderFactory = $this->getResponderFactory();
-        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher($Router, $ControllerFactory, $ResponderFactory);
+        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher($Router, $Logger, $ControllerFactory, $ResponderFactory);
         \ob_start();
         $Dispatcher->dispatchResponse($this->getRequest(''));
         $response = \ob_get_contents();
@@ -43,15 +50,24 @@ class FireDispatcherTest extends \PHPUnit_Framework_TestCase {
 
     public function testFireDispatcherWithInvalidAction() {
         $Router = $this->getRouter('');
+        $Logger = $this->Logger;
         $ControllerFactory = $this->getControllerFactory();
         $ResponderFactory = $this->getResponderFactory();
-        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher($Router, $ControllerFactory, $ResponderFactory);
+        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher($Router, $Logger, $ControllerFactory, $ResponderFactory);
         \ob_start();
         $Dispatcher->dispatchResponse($this->getRequest('/test-pages/non-existent'));
         $response = \ob_get_contents();
         \ob_end_clean();
-        $expected = '';
+        $expected = '<div>The action, nonExistent, was not found in, SprayFire.Test.Helpers.Controller.TestPages.</div>';
         $this->assertSame($expected, $response);
+        $expectedErrorMessages = array(
+            array(
+                'message' => 'The action, nonExistent, was not found in, SprayFire.Test.Helpers.Controller.TestPages.',
+                'options' => array()
+            )
+        );
+        $actualErrorMessage = $this->ErrorLog->getLoggedMessages();
+        $this->assertSame($expectedErrorMessages, $actualErrorMessage);
     }
 
     protected function getRequest($uri) {
@@ -71,7 +87,7 @@ class FireDispatcherTest extends \PHPUnit_Framework_TestCase {
     }
 
     protected function getControllerFactory() {
-        return new \SprayFire\Controller\Factory($this->Cache, $this->Container, $this->Logger, 'SprayFire.Controller.Controller', 'SprayFire.Test.Helpers.Controller.NullObject');
+        return new \SprayFire\Controller\Factory($this->Cache, $this->Container, $this->Logger, 'SprayFire.Controller.Controller', 'SprayFire.Test.Helpers.Controller.DispatchNullObject');
     }
 
     protected function getResponderFactory() {
