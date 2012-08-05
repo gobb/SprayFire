96a511cc6203a93c2e93ddb1d9e81ac93b781a06
Charles Sprayberry <cspray@gmail.com>
2012-08-05 12:54:05 -0400

    ADD fleshing out the SprayFire welcome page

---
+++ libs/SprayFire/Responder/html/index.php
@@ -0,0 +1,30 @@
+<div id="sprayfire-install-welcome">
+    <h2>Welcome to <span class="sprayfire-orange">Spray</span><span class="sprayfire-red">Fire</span>!</h2>
+    <p>Thanks for installing SprayFire, a fully unit tested framework designed to work with PHP 5.3.  Originally starting out as an academic project SprayFire has grown to include the following features.</p>
+    <ul>
+        <li>Feature 1</li>
+        <li>Feature 2</li>
+        <li>Feature 3</li>
+    </ul>
+</div>
+
+<div id="welcome-panels">
+    <div class="welcome-panel">
+         <div class="icon">
+            <a class="btn btn-large" href="#"><i title="View the project's documentation" class="icon-book"></i> API Docs</a>
+        </div>
+        <p class="summary">Read up on the API documentation.  Figure out what SprayFire is capable of and how your apps can take advantage of all that the framework has to offer.</p>
+    </div>
+    <div class="welcome-panel">
+        <div class="icon">
+            <a class="btn btn-large" href="//www.github.com/cspray/SprayFire"><i title="View the project on github" class="icon-github"></i> SprayFire on github</a>
+        </div>
+        <p class="summary">Fork, change, make a pull request.  Or raise an issue and check up on the SprayFire wiki.</p>
+    </div>
+    <div class="welcome-panel">
+        <div class="icon">
+            <a class="btn btn-large" href="#"><i title="See SprayFire users" class="icon-fire"></i> See SprayFire users</a>
+        </div>
+    </div>
+</div>
+
---
+++ libs/SprayFire/Responder/html/sidebar.php
@@ -0,0 +1,38 @@
+<div id="the-team">
+    <h2>meet the team</h2>
+    <div class="team-member">
+        <div class="bio">
+            <img alt="Charles Sprayberry - Benevolent Dicator for Life" src="http://www.gravatar.com/avatar/<?php echo $csprayGravatarHash; ?>" />
+            <ul>
+                <li>Name: Charles Sprayberry</li>
+                <li>Benevolent Dictator for Life</li>
+                <li>Lead developer and primary contributor to the SprayFire project.  Charles created the project in his spare time and spearheads the majority of the development efforts.  When Charles isn't coding or spending time with his fiancee, Dyana, he's perusing Netflix or working on his dog training skills.</li>
+            </ul>
+        </div>
+        <div class="social-networking">
+            <a href="//www.github.com/cspray/"><i class="icon-github-sign"></i></a>
+            <a href="//plus.google.com/u/0/102074471092306011068/posts"><i class="icon-google-plus-sign"></i></a>
+            <a href="//www.twitter.com/charlesspray/"><i class="icon-twitter-sign"></i></a>
+            <a href="//www.facebook.com/cspraysworld"><i class="icon-facebook-sign"></i></a>
+        </div>
+    </div>
+
+    <div class="team-member">
+        <div class="bio">
+            <img alt="Dyana Stewart - Graphic Designer" src="http://www.gravatar.com/avatar/<?php echo $dyanaGravatarHash; ?>" />
+            <ul>
+                <li>Name: Dyana Stewart</li>
+                <li>Graphic Designer</li>
+                <li></li>
+            </ul>
+        </div>
+        <div class="social-networking">
+            <a href="//github.com/Dyana"><i class="icon-github-sign"></i></a>
+            <a href="//plus.google.com/u/0/111996158059642637520/posts"><i class="icon-google-plus-sign"></i></a>
+            <a href="//twitter.com/dystewart249"><i class="icon-twitter-sign"></i></a>
+            <a href="//www.facebook.com/dyana.stewart1"><i class="icon-facebook-sign"></i></a>
+        </div>
+    </div>
+
+    <p>Have you made significant contributions to the project?  Are you wanting to be listed as a team member?  <a href="mailto:cspray@gmail.com">Mail Charles</a> and let him know!</p>
+</div>
