41c19ef09516f914803701fda8a934c20216a001
Charles Sprayberry <cspray@gmail.com>
2012-08-13 20:47:08 -0400

    UPD fixing dispatcher test to work with new design

--- libs/SprayFire/Test/Cases/Dispatcher/FireDispatcher/DispatcherTest.php
+++ libs/SprayFire/Test/Cases/Dispatcher/FireDispatcher/DispatcherTest.php
@@ -9,13 +9,9 @@ namespace SprayFire\Test\Cases\Dispatcher\FireDispatcher;
 
 class DispatcherTest extends \PHPUnit_Framework_TestCase {
 
-    protected $Cache;
-
     protected $Container;
 
-    protected $Logger;
-
-    protected $JavaNameConverter;
+    protected $environmentConfig;
 
     /**
      * @property SprayFire.Test.Helpers.DevelopmentLogger
@@ -23,29 +19,80 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase {
     protected $ErrorLog;
 
     public function setUp() {
-        $this->JavaNameConverter = new \SprayFire\JavaNamespaceConverter();
-        $this->Cache = new \SprayFire\ReflectionCache($this->JavaNameConverter);
-        $Emergency = $Debug = $Info = new \SprayFire\Test\Helpers\DevelopmentLogger();
-        $this->ErrorLog = $Error = new \SprayFire\Test\Helpers\DevelopmentLogger();
-        $this->Logger = new \SprayFire\Logging\FireLogging\LogDelegator($Emergency, $Error, $Debug, $Info);
+        $JavaNameConverter = new \SprayFire\JavaNamespaceConverter();
+        $ReflectionCache = new \SprayFire\ReflectionCache($JavaNameConverter);
+        $Container = new \SprayFire\Service\FireService\Container($ReflectionCache);
 
-        $this->Container = new \SprayFire\Service\FireService\Container($this->Cache, $this->JavaNameConverter);
-        $this->Container->addService('SprayFire.FileSys.FireFileSys.Paths', function () {
-            $RootPaths = new \SprayFire\FileSys\FireFileSys\RootPaths(\SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework');
-            return array($RootPaths);
-        });
-        $this->Container->addService($this->Logger);
+        $RootPaths = new \SprayFire\FileSys\FireFileSys\RootPaths(\SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework');
+        $Paths = new \SprayFire\FileSys\FireFileSys\Paths($RootPaths);
+
+        $Container->addService($JavaNameConverter);
+        $Container->addService($ReflectionCache);
+        $Container->addService($Paths);
+
+        $environmentConfig = array(
+            'services' => array(
+                'HttpRouter' => array(
+                    'name' => 'SprayFire.Http.Routing.FireRouting.Router',
+                    'parameterCallback' => function() use ($Paths) {
+                        $Normalizer = new \SprayFire\Http\Routing\FireRouting\Normalizer();
+                        $config = array(
+                            'routes' => array(
+                                '/' => array(
+                                    'namespace' => 'SprayFire.Test.Helpers.Controller',
+                                    'controller' => 'TestPages',
+                                    'action' => 'index-yo-dog',
+                                    'parameters' => array(
+                                        'yo',
+                                        'dog'
+                                    )
+                                ),
+                                '/test-pages/non-existent/' => array(
+                                    'namespace' => 'SprayFire.Test.Helpers.Controller',
+                                    'controller' => 'TestPages',
+                                    'action' => 'non-existent'
+                                )
+                            )
+                        );
+                        $installDir = \basename($Paths->getInstallPath());
+                        return array($Normalizer, $config, $installDir);
+                    }
+                ),
+                'Logging' => array(
+                    'name' => 'SprayFire.Logging.FireLogging.LogDelegator',
+                    'parameterCallback' => function() {
+                        $Logger = new \SprayFire\Logging\FireLogging\DevelopmentLogger();
+                        return array($Logger, $Logger, $Logger, $Logger);
+                    }
+                ),
+                'ControllerFactory' => array(
+                    'name' => 'SprayFire.Controller.FireController.Factory',
+                    'parameterCallback' => function() use ($ReflectionCache, $Container) {
+                        $Logger = $Container->getService('SprayFire.Logging.FireLogging.LogDelegator');
+                        return array($ReflectionCache, $Container, $Logger);
+                    }
+                ),
+                'ResponderFactory' => array(
+                    'name' => 'SprayFire.Responder.Factory',
+                    'parameterCallback' => function() use ($ReflectionCache, $Container) {
+                        $Logger = $Container->getService('SprayFire.Logging.FireLogging.LogDelegator');
+                        return array($ReflectionCache, $Container, $Logger);
+                    }
+                )
+            )
+        );
+
+        foreach ($environmentConfig['services'] as $service) {
+            $Container->addService($service['name'], $service['parameterCallback']);
+        }
+
+        $this->Container = $Container;
+        $this->environmentConfig = $environmentConfig;
     }
 
     public function testFireDispatcherTest() {
         $Request = $this->getRequest('');
-        $Router = $this->getRouter('');
-        $RoutedRequest = $Router->getRoutedRequest($Request);
-        $this->Container->addService($Request);
-        $Logger = $this->Logger;
-        $ControllerFactory = $this->getControllerFactory();
-        $ResponderFactory = $this->getResponderFactory();
-        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher\Dispatcher($Router, $this->Container, $Logger, $ControllerFactory, $ResponderFactory);
+        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher\Dispatcher($this->Container, $this->environmentConfig);
         \ob_start();
         $Dispatcher->dispatchResponse($Request);
         $response = \ob_get_contents();
@@ -54,68 +101,12 @@ class DispatcherTest extends \PHPUnit_Framework_TestCase {
         $this->assertSame($expected, $response);
     }
 
-    public function testFireDispatcherWithInvalidAction() {
-        $Request = $this->getRequest('/test-pages/non-existent');
-        $Router = $this->getRouter('');
-        $RoutedRequest = $Router->getRoutedRequest($Request);
-        $Logger = $this->Logger;
-        $this->Container->addService($Request);
-        $ControllerFactory = $this->getControllerFactory();
-        $ResponderFactory = $this->getResponderFactory();
-        $Dispatcher = new \SprayFire\Dispatcher\FireDispatcher\Dispatcher($Router, $this->Container, $Logger, $ControllerFactory, $ResponderFactory);
-        \ob_start();
-        $Dispatcher->dispatchResponse($Request);
-        $response = \ob_get_contents();
-        \ob_end_clean();
-        $expected = '<div>The action, nonExistent, was not found in, SprayFire.Test.Helpers.Controller.TestPages.</div>';
-        $this->assertSame($expected, $response);
-        $expectedErrorMessages = array(
-            array(
-                'message' => 'The action, nonExistent, was not found in, SprayFire.Test.Helpers.Controller.TestPages.',
-                'options' => array()
-            )
-        );
-        $actualErrorMessage = $this->ErrorLog->getLoggedMessages();
-        $this->assertSame($expectedErrorMessages, $actualErrorMessage);
-    }
-
     protected function getRequest($uri) {
         $_server = array();
         $_server['REQUEST_URI'] = $uri;
         $Uri = new \SprayFire\Http\FireHttp\Uri($_server);
         $Headers = new \SprayFire\Http\FireHttp\RequestHeaders();
         return new \SprayFire\Http\FireHttp\Request($Uri, $Headers);
-    }
-
-    protected function getRouter($installDir) {
-        $Normalizer = new \SprayFire\Http\Routing\FireRouting\Normalizer();
-        $config = array(
-            'routes' => array(
-                '/' => array(
-                    'namespace' => 'SprayFire.Test.Helpers.Controller',
-                    'controller' => 'TestPages',
-                    'action' => 'index-yo-dog',
-                    'parameters' => array(
-                        'yo',
-                        'dog'
-                    )
-                ),
-                '/test-pages/non-existent/' => array(
-                    'namespace' => 'SprayFire.Test.Helpers.Controller',
-                    'controller' => 'TestPages',
-                    'action' => 'non-existent'
-                )
-            )
-        );
-        return new \SprayFire\Http\Routing\FireRouting\Router($Normalizer, $config, $installDir);
-    }
-
-    protected function getControllerFactory() {
-        return new \SprayFire\Controller\FireController\Factory($this->Cache, $this->Container, $this->Logger, 'SprayFire.Controller.Controller', 'SprayFire.Test.Helpers.Controller.DispatchNullObject');
-    }
-
-    protected function getResponderFactory() {
-        return new \SprayFire\Responder\Factory($this->Cache, $this->Container, $this->Logger);
     }
 
 }
