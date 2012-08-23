b3c68fad56adf2c89f9dc2a4bb92ff9d94511caf
Charles Sprayberry <cspray@gmail.com>
2012-08-22 22:39:43 -0400

    UPD adding code to further test the app initializer

--- libs/SprayFire/Test/Cases/Dispatcher/FireDispatcher/AppInitializerTest.php
+++ libs/SprayFire/Test/Cases/Dispatcher/FireDispatcher/AppInitializerTest.php
@@ -9,18 +9,63 @@ namespace SprayFire\Test\Cases;
 
 class AppInitializerTest extends \PHPUnit_Framework_TestCase {
 
+    protected $Container;
+
+    protected $ClassLoader;
+
     public function setUp() {
 
     }
 
-    public function testAppInitializerTest() {
+    public function testAppInitializerRunningAppBootstrap() {
+        $Initializer = $this->getInitializer();
+        $controller = 'TestApp.Controller.Something';
+        $RoutedRequest = $this->getRoutedRequest($controller);
+        $Initializer->initializeApp($RoutedRequest);
+        $this->assertTrue($this->Container->doesServiceExist('TestApp.Service.FromBootstrap'), 'The service from the app bootstrap was not loaded');
+    }
 
+    public function testAppInitializerWithInvalidNamesapce() {
+        $Initializer = $this->getInitializer();
+        $controller = 'NonExistent.Controller';
+        $RoutedRequest = $this->getRoutedRequest($controller);
+        $this->setExpectedException('\\SprayFire\\Exception\\ResourceNotFound');
+        $Initializer->initializeApp($RoutedRequest);
+    }
 
+    public function testAppInitializerWithAppBootstrapNotBootstrapper() {
+        $Initializer = $this->getInitializer();
+        $controller = 'NoBootstrap.Bootstrap';
+        $RoutedRequest = $this->getRoutedRequest($controller);
+        $this->setExpectedException('\\SprayFire\\Exception\\ResourceNotFound');
+        $Initializer->initializeApp($RoutedRequest);
+    }
+
+    protected function getInitializer() {
+        $this->Container = $this->getServiceContainer();
+        $this->ClassLoader = $this->getClassLoader();
+        $this->ClassLoader->setAutoloader();
+        $installDir = \SPRAYFIRE_ROOT . '/libs/SprayFire/Test/mockframework';
+        $RootPaths = new \SprayFire\FileSys\FireFileSys\RootPaths($installDir);
+        $Paths = new \SprayFire\FileSys\FireFileSys\Paths($RootPaths);
+        return new \SprayFire\Dispatcher\FireDispatcher\AppInitializer($this->Container, $this->ClassLoader, $Paths);
     }
 
     protected function getServiceContainer() {
-        
+        $JavaNameConverter = new \SprayFire\JavaNamespaceConverter();
+        $ReflectionCache = new \SprayFire\ReflectionCache($JavaNameConverter);
+        return new \SprayFire\Service\FireService\Container($ReflectionCache);
     }
+
+    protected function getRoutedRequest($controller, $action = 'index', array $parameters = array(), $isStatic = false) {
+        return new \SprayFire\Http\Routing\FireRouting\RoutedRequest($controller, $action, $parameters, $isStatic);
+    }
+
+    protected function getClassLoader() {
+        return new \ClassLoader\Loader();
+    }
+
+
 
     public function tearDown() {
 
