ed089f07cf2aeb41db6f296fc636c3362f0e9725
Charles Sprayberry <cspray@gmail.com>
2012-08-22 22:41:17 -0400

    NEW a bootstrap file to test the AppInitializer

---
+++ libs/SprayFire/Test/mockframework/app/TestApp/Bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ *
+ * @author Charles Sprayberry
+ * @license Governed by the LICENSE file found in the root directory of this source
+ * code
+ */
+
+namespace TestApp;
+
+class Bootstrap extends \SprayFire\CoreObject implements \SprayFire\Bootstrap\Bootstrapper {
+
+    protected $Container;
+
+    protected $ClassLoader;
+
+    public function __construct(\SprayFire\Service\Container $Container, \ClassLoader\Loader $ClassLoader) {
+        $this->Container = $Container;
+        $this->ClassLoader = $ClassLoader;
+    }
+
+    public function runBootstrap() {
+        $this->Container->addService('TestApp.Service.FromBootstrap');
+    }
+
+
+}
