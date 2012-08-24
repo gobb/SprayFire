7da8dff6c4a9bfbdfb11ae0de6845054cc5c5d6d
Charles Sprayberry <cspray@gmail.com>
2012-08-24 07:50:05 -0400

    UPD adding test to make sure anonymous functions are being invoked

--- libs/SprayFire/Test/Cases/Mediator/FireMediator/CallbackTest.php
+++ libs/SprayFire/Test/Cases/Mediator/FireMediator/CallbackTest.php
@@ -19,6 +19,30 @@ class CallbackTest extends \PHPUnit_Framework_TestCase {
         $this->assertSame($eventName, $Callback->getEventName());
     }
 
+    public function testCallbackInvokingAnonymousFunction() {
+
+        $testData = array();
+        $eventName = \SprayFire\Mediator\DispatcherEvents::BEFORE_CONTROLLER_INVOKED;
+
+        $function = function(\SprayFire\Mediator\Event $Event) use(&$testData) {
+            $testData['eventName'] = $Event->getEventName();
+            $testData['target'] = $Event->getTarget();
+            $testData['arguments'] = $Event->getArguments();
+        };
+
+        $Callback = new \SprayFire\Mediator\FireMediator\Callback($eventName, $function);
+
+        $target = 'test';
+        $arguments = array(1,2,3,4);
+        $Event = new \SprayFire\Mediator\FireMediator\Event($eventName, $target, $arguments);
+
+        $Callback->invoke($Event);
+        $this->assertSame($target, $testData['target']);
+        $this->assertSame($eventName, $testData['eventName']);
+        $this->assertSame($arguments, $testData['arguments']);
+
+    }
+
     public function tearDown() {
 
     }
