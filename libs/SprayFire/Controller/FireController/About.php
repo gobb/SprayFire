998793c61436fd4651901f804df7a0c6a50fb429
Charles Sprayberry <cspray@gmail.com>
2012-09-22 12:26:53 -0400

    UPD refactoring file level doc block, adding class doc block, updating method doc blocks

--- libs/SprayFire/Controller/FireController/About.php
+++ libs/SprayFire/Controller/FireController/About.php
@@ -1,26 +1,54 @@
 <?php
 
 /**
+ * Implementation of SprayFire.Controller.Controller provided in the default SprayFire
+ * install
  *
  * @author Charles Sprayberry
- * @license Governed by the LICENSE file found in the root directory of this source
- * code
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since 0.1
  */
 
 namespace SprayFire\Controller\FireController;
 
 use \SprayFire\Controller\FireController\Base as BaseController;
 
+/**
+ * This implementation is intended to provide the user some more detailed info on
+ * SprayFire as well as its creator and contributors.
+ *
+ * This controller is not required to stick around, but please remove the appropriate
+ * route, '/about/', if you decide to remove this from your SprayFire install.
+ */
 class About extends BaseController implements \SprayFire\Controller\Controller {
 
+    /**
+     * A default service provided by SprayFire.Controller.FireController.Base.
+     *
+     * This property is here as a convenience to not need to call $this->service()
+     * everytime we need to use the service.
+     *
+     * @property SprayFire.FileSys.PathGenerator
+     */
     protected $Paths;
 
-    public function setUp() {
+    /**
+     * Takes care of some generic actions that may be used by multiple actions
+     *
+     * @return void
+     */
+    protected function setUp() {
         $this->Paths = $this->service('Paths');
         $this->layoutPath = $this->Paths->getLibsPath('SprayFire', 'Responder', 'html', 'layout', 'default.php');
         $this->templatePath = $this->Paths->getLibsPath('SprayFire', 'Responder', 'html', 'about.php');
     }
 
+    /**
+     * Prepares data needed to let the user know more about the framework.
+     *
+     * @return void
+     */
     public function sprayFire() {
         $this->setUp();
         $messages = array(
