39f933a56c4884be8b1e1765d9c8a9f93b33974e
Charles Sprayberry <cspray@gmail.com>
2012-09-21 20:55:19 -0400

    UPD refactoring file level doc block, adding class doc block, adding doc block to method

--- libs/SprayFire/Bootstrap/NullObject.php
+++ libs/SprayFire/Bootstrap/NullObject.php
@@ -1,11 +1,10 @@
 <?php
 
 /**
- * A Null bootstrap to be returned from a Bootstrap Factory
+ * Implementation of SprayFire.Bootstrap.Bootstrapper that performs no operations
  *
- * @author Charles Sprayberry
- * @license Governed by the LICENSE file found in the root directory of this source
- * code
+ * @author   Charles Sprayberry
+ * @license  All code subject to the terms of the LICENSE file in the project root
  */
 
 namespace SprayFire\Bootstrap;
@@ -13,8 +12,17 @@ namespace SprayFire\Bootstrap;
 use \SprayFire\Bootstrap\Bootstrapper as Bootstrapper,
     \SprayFire\CoreObject as CoreObject;
 
+/**
+ * The primary use case for this object would be as a possible return value from
+ * a factory that creates SprayFire.Bootstrap.Bootstrapper objects.
+ */
 class NullObject extends CoreObject implements Bootstrapper {
 
+    /**
+     * Performs no operation and returns null.
+     *
+     * @return null
+     */
     public function runBootstrap() {
         return null;
     }
