11fcc5c78d03a009963d92f2f9f0093660713833
Charles Sprayberry <cspray@gmail.com>
2012-09-23 11:41:48 -0400

    UPD updating documentation, returning appropriate data types from those interfaces expecting it

--- libs/SprayFire/Controller/NullObject.php
+++ libs/SprayFire/Controller/NullObject.php
@@ -1,12 +1,13 @@
 <?php
 
 /**
- * A default controller returned from the controller factory if the original controller
- * requested could not be located, ie a 404 response.
+ * An implementation of SprayFire.Controller.Controller that should perform no
+ * operations.
  *
- * @author Charles Sprayberry
- * @license Governed by the LICENSE file found in the root directory of this source
- * code
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
  */
 
 namespace SprayFire\Controller;
@@ -14,6 +15,22 @@ namespace SprayFire\Controller;
 use \SprayFire\Controller\Controller as Controller,
     \SprayFire\CoreObject as CoreObject;
 
+/**
+ * By default this object is returned from the SprayFire.Controller.FireController.Factory
+ * implementation and can be used as a 404 object, controlling what is displayed
+ * when the appropriate resource could not be found.
+ *
+ * This object does not allow the supporting of services and does not extend
+ * SprayFire.Controller.FireController.Base by design.  This is intentionally
+ * supposed to be as minimal as possible and to perform no operations for the
+ * majority of calls.
+ *
+ * By default this object will only return values for
+ *
+ * - getResponderName() // SprayFire.Responder.FireResponder.Html
+ * - getLayoutPath()    // install_dir/Responder/html/default.php
+ * - getTemplatePath()  // install_dir/Responder/html/blank.php
+ */
 class NullObject extends CoreObject implements Controller {
 
     /**
@@ -28,38 +45,86 @@ class NullObject extends CoreObject implements Controller {
 
     }
 
+    /**
+     * No operation; returns empty array
+     *
+     * @return array
+     */
     public function getCleanData() {
         return array();
     }
 
+    /**
+     * No operation; returns empty array
+     *
+     * @return array
+     */
     public function getDirtyData() {
         return array();
     }
 
+    /**
+     * Returns a layout path to be used by a SprayFire.Responder.FireResponder.Html
+     * object.
+     *
+     * @return string
+     */
     public function getLayoutPath() {
         return \dirname(__DIR__) . '/Responder/html/layout/default.php';
     }
 
+    /**
+     * Returns the default HTML responder provided by the framework
+     *
+     * @return string
+     */
     public function getResponderName() {
-        return '';
+        return 'SprayFire.Responder.FireResponder.Html';
     }
 
+    /**
+     * Returns a template path to be used by a SprayFire.Responder.FireResponder.Html
+     * object.
+     *
+     * @return string
+     */
     public function getTemplatePath() {
         return \dirname(__DIR__) . '/Responder/html/blank.php';
     }
 
+    /**
+     * No operation; no data will be provided to layout or template
+     *
+     * @param array $data
+     */
     public function giveCleanData(array $data) {
 
     }
 
+    /**
+     * No operation; no data will be provided to layout or template
+     *
+     * @param array $data
+     */
     public function giveDirtyData(array $data) {
 
     }
 
+    /**
+     * No operation; returns an empty array
+     *
+     * @return array
+     */
     public function getRequestedServices() {
-
+        return array();
     }
 
+    /**
+     * No operation; no service is provided
+     *
+     * @param string $key
+     * @param object $Service
+     */
     public function giveService($key, $Service) {
 
     }
