dc2db2228e5794107bccf141063b958cc7badc4d
Charles Sprayberry <cspray@gmail.com>
2012-09-26 07:49:22 -0400

    UPD refactoring file and class doc block

--- libs/SprayFire/Dispatcher/AppInitializer.php
+++ libs/SprayFire/Dispatcher/AppInitializer.php
@@ -1,9 +1,8 @@
 <?php
 
 /**
- * Interface designed to get an application, as based on information from
- * SprayFire.Http.Routing.RoutedRequest, started up and its bootstrap is initiated.
- *
+ * Interface designed to get an application initialized based on the results
+ * of routing passed via SprayFire.Http.Routing.RoutedRequest
  *
  * @author  Charles Sprayberry
  * @license Subject to the terms of the LICENSE file in the project root
@@ -16,6 +15,14 @@ namespace SprayFire\Dispatcher;
 use \SprayFire\Http\Routing as SFRouting;
 
 /**
+ * Implementations of this interface should take care of whatever is needed to
+ * get an application booted up so that it may take whatever actions are necessary
+ * to process a request.
+ *
+ * At the bare minimum this should include setting up autoloading for the application
+ * and, if applicable, instantiate and run any SprayFire.Bootstrap.Bootstrappers
+ * that may be assigned to the application.
+ *
  * @package SprayFire
  * @subpackage Dispatcher
  */
