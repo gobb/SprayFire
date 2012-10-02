4b47432b9f314827e360b4c7ca1d2c6714cc1df8
Charles Sprayberry <cspray@gmail.com>
2012-10-02 19:58:51 -0400

    DEL & ADD renaming LogDelegator implementation to LogOverseer

--- libs/SprayFire/Logging/FireLogging/LogOverseer.php
+++ libs/SprayFire/Logging/FireLogging/LogOverseer.php
@@ -1,38 +1,60 @@
 <?php
 
 /**
- * Holds an implementation of SprayFire.Logging.LogOverseer to manage the destination
- * of specific log messages.
+ * Implementation of SprayFire.Logging.LogOverseer that uses other FireLogging
+ * implementations to provide functionality.
  *
- * @author Charles Sprayberry
- * @license Governed by the LICENSE file found in the root directory of this source
- * code
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
  */
 
 namespace SprayFire\Logging\FireLogging;
 
-use \SprayFire\Logging\LogOverseer as LogOverseer,
-    \SprayFire\Logging\Logger as Logger,
-    \SprayFire\CoreObject as CoreObject;
+use \SprayFire\Logging as SFLogging,
+    \SprayFire\CoreObject as SFCoreObject;
 
-class LogDelegator extends CoreObject implements LogOverseer {
+/**
+ * Provides the ability to log information at a variety of levels and sources.
+ *
+ * @package SprayFire
+ * @subpackage Logging.FireLogging
+ *
+ * @todo
+ * We need to create a test to test getting a logger and ensuring that the
+ * appropriate loggers are used for the appropriate calls.
+ */
+class LogOverseer extends SFCoreObject implements SFLogging\LogOverseer {
 
     /**
+     * This SprayFire.Logging.Logger can be retrieved from LogOverseer::getLogger()
+     * by passing SprayFire.Logging.LogOverseer::EMERGENCY_LOGGER.
+     *
      * @property SprayFire.Logging.Logger
      */
     protected $EmergencyLogger;
 
     /**
+     * This SprayFire.Logging.Logger can be retrieved from LogOverseer::getLogger()
+     * by passing SprayFire.Logging.LogOverseer::ERROR_LOGGER.
+     *
      * @property SprayFire.Logging.Logger
      */
     protected $ErrorLogger;
 
     /**
+     * This SprayFire.Logging.Logger can be retrieved from LogOverseer::getLogger()
+     * by passing SprayFire.Logging.LogOverseer::DEBUG_LOGGER
+     *
      * @property SprayFire.Logging.Logger
      */
     protected $DebugLogger;
 
     /**
+     * This SprayFire.Logging.Logger can be retrieved from LogOverseer::getLogger()
+     * by passing SprayFire.Logging.LogOverseer::INFO_LOGGER
+     *
      * @property SprayFire.Logging.Logger
      */
     protected $InfoLogger;
@@ -43,7 +65,12 @@ class LogDelegator extends CoreObject implements LogOverseer {
      * @param SprayFire.Logging.Logger $Debug
      * @param SprayFire.Logging.Logger $Info
      */
-    public function __construct(Logger $Emergency, Logger $Error, Logger $Debug, Logger $Info) {
+    public function __construct(
+        SFLogging\Logger $Emergency,
+        SFLogging\Logger $Error,
+        SFLogging\Logger $Debug,
+        SFLogging\Logger $Info
+    ) {
         $this->EmergencyLogger = $Emergency;
         $this->ErrorLogger = $Error;
         $this->DebugLogger = $Debug;
@@ -86,19 +113,26 @@ class LogDelegator extends CoreObject implements LogOverseer {
         return $this->InfoLogger->log($message, $options);
     }
 
+    /**
+     * Will return a SprayFire.Logging.Logger if the passed $loggerType is valid
+     * or null if an invalid type is passed to $loggerType.
+     *
+     * @param string $loggerType
+     * @return mixed
+     */
     public function getLogger($loggerType) {
         $loggerType = \strtolower($loggerType);
         switch($loggerType) {
-            case 'emergency':
+            case SFLogging\LogOverseer::EMERGENCY_LOGGER:
                 return $this->EmergencyLogger;
                 break;
-            case 'error':
+            case SFLogging\LogOverseer::ERROR_LOGGER:
                 return $this->ErrorLogger;
                 break;
-            case 'info':
+            case SFLogging\LogOverseer::INFO_LOGGER:
                 return $this->InfoLogger;
                 break;
-            case 'debug':
+            case SFLogging\LogOverseer::DEBUG_LOGGER:
                 return $this->DebugLogger;
                 break;
             default:
