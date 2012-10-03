98e7b1162ed4216e064739465cdd2d02da69946e
Charles Sprayberry <cspray@gmail.com>
2012-10-03 07:43:47 -0400

    UPD creating an Exception namespace under Service, putting ServiceNotFound into it

---
+++ libs/SprayFire/Service/Exception/ServiceNotFound.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Exception thrown by SprayFire.Service.Container implementations.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Service\Exception;
+
+/**
+ * Should be thrown by implementations of SprayFire.Service.Container when a service
+ * is attempted to be retrieved that was not properly added to the container.
+ *
+ * @package SprayFire
+ * @subpackage Service.Exception
+ */
+class ServiceNotFound extends \Exception {}
