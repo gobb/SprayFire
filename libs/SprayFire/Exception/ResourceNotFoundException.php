a7c70ec95e8339f8ccb45711697d7d0cbbdc0873
Charles Sprayberry <cspray@gmail.com>
2012-10-03 21:30:23 -0400

    DEL & ADD renaming ResourceNotFound -> ResourceNotFoundException

---
+++ libs/SprayFire/Exception/ResourceNotFoundException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Thrown if a class or interface type could not be found.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Exception;
+
+/**
+ * @package SprayFire
+ * @subpackage Exception
+ */
+class ResourceNotFoundException extends \RuntimeException {}
