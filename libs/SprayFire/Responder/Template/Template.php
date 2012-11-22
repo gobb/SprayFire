c9231b6ea207bde26cdca6907d3d667a2648b131
Charles Sprayberry <cspray@gmail.com>
2012-11-22 10:33:52 -0500

    NEW initial API for object oriented templates

---
+++ libs/SprayFire/Responder/Template/Template.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * An interface that allows the abstraction of templates that deliver the appropriate
+ * content for a response.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Responder\Template;
+
+use \SprayFire\Object as SFObject;
+
+/**
+ * @package SprayFire
+ * @subpackage Responder.Template
+ */
+interface Template extends SFObject {
+
+    /**
+     * Return a value that can be used to uniquely identify this template.
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Return the fully rendered template based on the $data passed.
+     *
+     * @param array $data
+     * @return string
+     */
+    public function getContent(array $data);
+
+}
