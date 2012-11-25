01898101e109402de751717c33f5a7374ff76040
Charles Sprayberry <cspray@gmail.com>
2012-11-25 17:01:43 -0500

    ADD exception thrown if a template stored in a file could not be found or opened

---
+++ libs/SprayFire/Responder/Template/Exception/FileNotFound.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Thrown if a file path to a file template could not be properly found or opened.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Responder\Template\Exception;
+
+/**
+ *
+ *
+ * @package SprayFire
+ * @subpackage`
+ */
+class FileNotFound extends \RuntimeException {
+
+}
