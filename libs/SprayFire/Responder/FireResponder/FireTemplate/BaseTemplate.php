cbc5533bd37226e84bd7b0fe2f6d2f39a88f307d
Charles Sprayberry <cspray@gmail.com>
2012-11-25 12:51:19 -0500

    ADD A Base \Responder\Template\Template to provide getName() functionality

---
+++ libs/SprayFire/Responder/FireResponder/FireTemplate/BaseTemplate.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Abstract class to provide basic getName() support across a variety of templates
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Responder\FireResponder\FireTemplate;
+
+use \SprayFire\Responder\Template as SFResponderTemplate,
+    \SprayFire\CoreObject as SFCoreObject;
+
+/**
+ * @package SprayFire
+ * @subpackage`Responder.FireResponder.FireTemplate
+ */
+abstract class BaseTemplate extends SFCoreObject implements SFResponderTemplate\Template {
+
+    /**
+     * The name for the template
+     *
+     * @property string
+     */
+    protected $name;
+
+    /**
+     * @param string $name
+     */
+    public function __construct($name) {
+        $this->name = (string) $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+}
