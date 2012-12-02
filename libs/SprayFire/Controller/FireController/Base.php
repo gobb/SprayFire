892e7255ad15fa2b0df13154a081cff4ebc502bf
Charles Sprayberry <cspray@gmail.com>
2012-12-02 14:04:47 -0500

    UPD removing unnecessary code with Service Consumer changes

--- libs/SprayFire/Controller/FireController/Base.php
+++ libs/SprayFire/Controller/FireController/Base.php
@@ -51,39 +51,6 @@ abstract class Base extends FireService\Consumer implements SFController\Control
     protected $responderData = array();
 
     /**
-     * Array of services provided to this SprayFire.Controller.Controller during
-     * creation by the Controller Factory as defined by Base::$services
-     *
-     * @property array
-     */
-    protected $attachedServices = array();
-
-    /**
-     * @property SprayFire.FileSys.PathGenerator
-     */
-    protected $Paths;
-
-    /**
-     * @property SprayFire.Http.Request
-     */
-    protected $Request;
-
-    /**
-     * @property SprayFire.Http.Routing.RoutedRequest
-     */
-    protected $RoutedRequest;
-
-    /**
-     * @property SprayFire.Logging.LogOverseer
-     */
-    protected $Logging;
-
-    /**
-     * @property SprayFire.Responder.Template.Manager
-     */
-    protected $TemplateManager;
-
-    /**
      * Array of services that is provided by default to all implementations extending
      * this class.
      *
@@ -105,12 +72,7 @@ abstract class Base extends FireService\Consumer implements SFController\Control
      * @return void
      */
     public function beforeAction(SFMediator\Event $Event) {
-        $properties = \get_object_vars($this);
-        foreach ($this->services as $serviceKey => $serviceName) {
-            if (\array_key_exists($serviceKey, $properties)) {
-                $this->$serviceKey = $this->service($serviceKey);
-            }
-        }
+
     }
 
     /**
