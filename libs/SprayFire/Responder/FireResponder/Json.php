01d4cafdd0b39027163da3e9bba99af9a3aff21f
Charles Sprayberry <cspray@gmail.com>
2012-12-03 21:26:30 -0500

    ADD passing tests

---
+++ libs/SprayFire/Responder/FireResponder/Json.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * An implementation of SprayFire.Responder.Responder that will generate a JSON
+ * response for a given request.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Responder\FireResponder;
+
+use \SprayFire\Responder as SFResponder,
+    \SprayFire\Controller as SFController,
+    \SprayFire\Service\FireService as FireService;
+
+/**
+ * @package SprayFire
+ * @subpackage Responder.FireResponder
+ */
+class Json extends FireService\Consumer implements SFResponder\Responder {
+
+    /**
+     * 
+     *
+     * @param SprayFire.Controller.Controller $Controller
+     */
+    public function generateDynamicResponse(SFController\Controller $Controller) {
+        $data = $Controller->getResponderData();
+        echo \json_encode($data);
+    }
+
+}
