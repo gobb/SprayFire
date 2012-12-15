74f5c600723f29aee0deb035fb6f331353e4fbe6
Charles Sprayberry <cspray@gmail.com>
2012-12-15 12:43:50 -0500

    ADD initial implementation to get tests passing

---
+++ libs/SprayFire/Validation/Check/FireCheck/Check.php
@@ -0,0 +1,130 @@
+<?php
+
+/**
+ * Default implementation of SprayFire.Validation.Check.Check
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Validation\Check\FireCheck;
+
+use \SprayFire\Validation\Check as SFValidationCheck,
+    \SprayFire\CoreObject as SFCoreObject;
+
+/**
+ * 
+ *
+ *
+ * @package SprayFire
+ * @subpackage`Validation.Check.FireCheck
+ */
+abstract class Check extends SFCoreObject implements SFValidationCheck\Check {
+
+    /**
+     * Error code used if the $errorCode passed to various functions are null
+     */
+    const DEFAULT_ERROR_CODE = 1;
+
+    /**
+     * Will format the appropriate messages to support tokens
+     *
+     * @property SprayFire.Validation.Check.MessageParser
+     */
+    protected $MessageParser;
+
+    /**
+     * Map of tokens => values that should be used when parsing log and display
+     * messages.
+     *
+     * @property array
+     */
+    protected $tokenValues = array();
+
+    /**
+     * Map of errorCode => logMessage values.
+     *
+     * @property array
+     */
+    protected $logMessages = array();
+
+    /**
+     * Map of errorCode => displayMessage values
+     *
+     * @property array
+     */
+    protected $displayMessages = array();
+
+    /**
+     * @param SprayFire.Validation.Check.MessageParser $MessageParser
+     */
+    public function __construct(SFValidationCheck\MessageParser $MessageParser) {
+        $this->MessageParser = $MessageParser;
+    }
+
+    /**
+     * Will return an array of 'log' and 'display' messages matching the passed
+     * $errorCode.
+     *
+     * If no error code is passed the Check::DEFAULT_ERROR_CODE will be used.
+     *
+     * @param integer $errorCode
+     * @return array
+     */
+    public function getMessages($errorCode = null) {
+        $errorCode = $this->getErrorCode($errorCode);
+        $log = isset($this->logMessages[$errorCode]) ? $this->logMessages[$errorCode] : '';
+        $display = isset($this->displayMessages[$errorCode]) ? $this->displayMessages[$errorCode] : '';
+        return \compact('log', 'display');
+    }
+
+    /**
+     * @param string $message
+     * @param integer $errorCode
+     */
+    public function setLogMessage($message, $errorCode = null) {
+        $errorCode = $this->getErrorCode($errorCode);
+        $this->logMessages[$errorCode] = (string) $message;
+    }
+
+    /**
+     * @param string $message
+     * @param integer $errorCode
+     */
+    public function setDisplayMessage($message, $errorCode = null) {
+        $errorCode = $this->getErrorCode($errorCode);
+        $this->displayMessages[$errorCode] = (string) $message;
+    }
+
+    /**
+     * Should return an array of [token => value] used for log and display messages.
+     *
+     * @return array
+     */
+    abstract protected function getTokenValues();
+
+    /**
+     * Helper method that could be used to implement getTokenValues simply as
+     * return $this->tokenValues and using this method to set all token values.
+     *
+     * @param string $token
+     * @param string $value
+     */
+    protected function setTokenValue($token, $value) {
+        $this->tokenValues[(string) $token] = (string) $value;
+    }
+
+    /**
+     * Will return the passed $errorCode unless it is not set in which case the
+     * default error code will be returned.
+     *
+     * @param integer $errorCode
+     * @return integer
+     */
+    protected function getErrorCode($errorCode) {
+        return isset($errorCode) ? (int) $errorCode : self::DEFAULT_ERROR_CODE;
+    }
+
+}
