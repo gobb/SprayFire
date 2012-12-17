6c853d35fa629dfa1ecf0149d122ec1a7a929e51
Charles Sprayberry <cspray@gmail.com>
2012-12-17 12:30:31 -0500

    ADD interface to generate validation rules for a set of data

---
+++ libs/SprayFire/Validation/Rules.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Interface to determine how a set of data should be validated
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+
+namespace SprayFire\Validation;
+
+use \SprayFire\Validation\Check as SFValidationCheck,
+    \SprayFire\Object as SFObject;
+
+/**
+ * @package SprayFire
+ * @subpackage Validation
+ */
+interface Rules extends SFObject {
+
+    /**
+     * Adds a $Check to the end of the chain for a given $field, if $breakOnFailure
+     * is true if the given $Check does not pass the $field value the chain will
+     * stop with this check.
+     *
+     * $Check should be ran in the order they are added to the Rules
+     *
+     * @param string $field
+     * @param SprayFire.Validation.Check.Check $Check
+     * @param boolean $breakOnFailure
+     */
+    public function addCheck($field, SFValidationCheck\Check $Check, $breakOnFailure = false);
+
+    /**
+     * Return a Traversable structure that communicates what order of $Checks to
+     * run and whether a $Check should break on failure.
+     *
+     * @param string $field
+     * @return Traversable
+     */
+    public function getChecks($field);
+
+}
