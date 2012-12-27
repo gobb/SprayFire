206e8197713f0ccb8d25491fd0fe20a4a7261e76
Charles Sprayberry <cspray@gmail.com>
2012-12-26 22:43:47 -0500

    UPD adding docs

--- libs/SprayFire/Dispatcher/Exception/BootstrapNotFound.php
+++ libs/SprayFire/Dispatcher/Exception/BootstrapNotFound.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- *
+ * Thrown if an application's bootstrap could not be appropriately found.
  *
  * @author  Charles Sprayberry
  * @license Subject to the terms of the LICENSE file in the project root
@@ -13,8 +13,6 @@ namespace SprayFire\Dispatcher\Exception;
 use \RuntimeException as RuntimeException;
 
 /**
- *
- *
  * @package SprayFire
  * @subpackage Dispatcher.Exception
  */
