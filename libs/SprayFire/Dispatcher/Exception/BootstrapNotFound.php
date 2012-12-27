6f3e121f1f7b6d5b06503a18f5b550db581c386e
Charles Sprayberry <cspray@gmail.com>
2012-12-26 22:36:11 -0500

    ADD a specific exception when AppInitializer could not find an appropriate bootstrap

---
+++ libs/SprayFire/Dispatcher/Exception/BootstrapNotFound.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ *
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+namespace SprayFire\Dispatcher\Exception;
+
+use \RuntimeException as RuntimeException;
+
+/**
+ *
+ *
+ * @package SprayFire
+ * @subpackage Dispatcher.Exception
+ */
+class BootstrapNotFound extends RuntimeException {
+
+}
