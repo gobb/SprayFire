ed9be0499ce95ee1ab6203c7734fccca032f52c4
Charles Sprayberry <cspray@gmail.com>
2012-12-28 23:27:35 -0500

    ADD initial test for alphanumeric string with no spaces

---
+++ libs/SprayFire/Test/Cases/Validation/FireCheck/AlphanumericTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+namespace SprayFire\Test\Cases\Validation\FireCheck;
+
+use \SprayFire\Validation\Check\FireCheck as FireCheck,
+    \PHPUnit_Framework_TestCase as PHPUnitTestCase;
+
+/**
+ *
+ *
+ * @package SprayFire
+ * @subpackage Test.Cases.Validation.FireCheck
+ */
+class AlphanumericTest extends PHPUnitTestCase {
+
+    public function testAlphanumericReturningNoErrorWithoutSpaces() {
+        $Check = FireCheck\Alphanumeric();
+        $code = $Check->passesCheck('alph4b3ticstringwith4umbers');
+        $this->assertSame(FireCheck\ErrorCodes::NO_ERROR, $code);
+    }
+
+}
