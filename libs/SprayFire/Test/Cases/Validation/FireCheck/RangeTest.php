8d52724791314d3106119b2999b391c47da03024
Charles Sprayberry <cspray@gmail.com>
2012-12-30 19:46:05 -0500

    ADD initial test for a Range check

---
+++ libs/SprayFire/Test/Cases/Validation/FireCheck/RangeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Testing a Range test to ensure appropriate error codes are returned and respecting
+ * clusivity is honored.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+namespace SprayFire\Test\Cases\Validation\FireCheck;
+
+use \SprayFire\Validation\Check\FireCheck as FireCheck,
+    \PHPUnit_Framework_TestCase as PHPUnitTestCase;
+
+/**
+ * @package SprayFireTest
+ * @subpackage Cases.Validation.FireCheck
+ */
+class RangeTest extends PHPUnitTestCase {
+
+    /**
+     * Ensures that a value between the range is properly returned with NO_ERROR
+     * with defaulting to an exclusive check.
+     */
+    public function testRangeMinAndMaxDefaultSettings() {
+        $Check = new FireCheck\Range(1, 10);
+        $code = $Check->passesCheck(2);
+        $this->assertSame(FireCheck\ErrorCodes::NO_ERROR, $code);
+    }
+
+}
