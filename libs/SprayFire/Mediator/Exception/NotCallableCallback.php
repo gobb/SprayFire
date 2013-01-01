bcc4f6e68f7b3a1d8a17f23cd20511bce95a6989
Charles Sprayberry <cspray@gmail.com>
2013-01-01 12:50:11 -0500

    ADD exception thrown for not callable callback

---
+++ libs/SprayFire/Mediator/Exception/NotCallableCallback.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * An exception thrown if a not callable parameter is passed to a Callback object
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+namespace SprayFire\Mediator\Exception;
+
+use \InvalidArgumentException as InvalidArgumentException;
+
+/**
+ *
+ *
+ * @package SprayFire
+ * @subpackage Mediator.Exception
+ */
+class NotCallableCallback extends InvalidArgumentException {
+
+}
