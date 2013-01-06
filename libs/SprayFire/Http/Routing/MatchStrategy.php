556d0ad175c305dc2251ec281d8dcc151fddb619
Charles Sprayberry <cspray@gmail.com>
2013-01-05 21:57:01 -0500

    UPD adding constants for array keys for return value

--- libs/SprayFire/Http/Routing/MatchStrategy.php
+++ libs/SprayFire/Http/Routing/MatchStrategy.php
@@ -21,12 +21,27 @@ use \SprayFire\Http as SFHttp,
 interface MatchStrategy extends SFObject {
 
     /**
+     * A value that should be used as the array key for the Route object in return
+     * values from the getRouteAndParameters method.
+     */
+    const ROUTE_KEY = 'Route';
+
+    /**
+     * A value that should be used as the array key for the parameters in return
+     * values from the getRouteAndParameters method.
+     */
+    const PARAMETER_KEY = 'parameters';
+
+    /**
      * Matches a $Request to a \SprayFire\Http\Routing\Route stored in the $Bag
      * or otherwise creates a Route implementation to be used during routing.
      *
      * Please note that the array returned should have 2 keys:
-     * - Route => The Route object matched
-     * - parameters => an array of parameters to pass to the action
+     * - MatchStrategy::ROUTE_KEY => The Route object matched
+     * - MatchStrategy::PARAMETER_KEY => an array of parameters to pass to the action
+     *
+     * It is strongly recommended that you use the constants provided by this
+     * interface when setting the keys in the array returned from this method.
      *
      * @param \SprayFire\Http\Routing\RouteBag $Bag
      * @param \SprayFire\Http\Request $Request
