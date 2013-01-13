f05310407f995fd659cac03afe14f0d8ac2c4ff2
Charles Sprayberry <cspray@gmail.com>
2013-01-13 18:32:57 -0500

    UPD fixing link for logo to root path

--- libs/SprayFire/Responder/html/layout/default.php
+++ libs/SprayFire/Responder/html/layout/default.php
@@ -11,7 +11,7 @@
             <div id="content">
                 <div id="header">
                     <div id="main-header">
-                        <h1><a href="/SprayFire"><img src="<?php echo $Responder->Paths->getUrlPath('images', 'sprayfire-logo-bar-75.png'); ?>" id="sprayfire-logo" alt="SprayFire logo" width="200" height="75" /></a> A PHP 5.3+ Framework</h1>
+                        <h1><a href="/"><img src="<?php echo $Responder->Paths->getUrlPath('images', 'sprayfire-logo-bar-75.png'); ?>" id="sprayfire-logo" alt="SprayFire logo" width="200" height="75" /></a> A PHP 5.3+ Framework</h1>
                     </div>
                 </div>
 
