086921f3862b09b275932d4f2eba9bf373972e7a
Charles Sprayberry <cspray@gmail.com>
2013-02-01 20:32:14 -0500

    UPD adding framework processing into function to prevent work being added to global scope

--- init.php
+++ init.php
@@ -12,109 +12,115 @@ use \SprayFire\FileSys\FireFileSys as FireFileSys,
     \SprayFire\Logging\FireLogging as FireLogging,
     \SprayFire\Utils as SFUtils;
 
-$requestStartTime = \microtime(true);
+function startProcessing() {
+    $requestStartTime = \microtime(true);
 
-$installPath = __DIR__;
-$libsPath = $installPath . '/libs';
-$appPath = $installPath . '/app';
-$webPath = $installPath . '/web';
-$configPath = $installPath . '/config';
-$logsPath = $installPath . '/logs';
+    $installPath = __DIR__;
+    $libsPath = $installPath . '/libs';
+    $appPath = $installPath . '/app';
+    $webPath = $installPath . '/web';
+    $configPath = $installPath . '/config';
+    $logsPath = $installPath . '/logs';
 
-include $libsPath . '/ClassLoader/Loader.php';
-$ClassLoader = new \ClassLoader\Loader();
-$ClassLoader->registerNamespaceDirectory('SprayFire', $libsPath);
-$ClassLoader->registerNamespaceDirectory('Zend', $libsPath);
-$ClassLoader->setAutoloader();
+    include $libsPath . '/ClassLoader/Loader.php';
+    $ClassLoader = new \ClassLoader\Loader();
+    $ClassLoader->registerNamespaceDirectory('SprayFire', $libsPath);
+    $ClassLoader->registerNamespaceDirectory('Zend', $libsPath);
+    $ClassLoader->setAutoloader();
 
-$getEnvironmentConfig = function() use($configPath) {
-    $path = $configPath . '/SprayFire/environment.php';
-    $config = array();
-    if (\file_exists($path)) {
-        $config = (array) include $path;
+    $getEnvironmentConfig = function() use($configPath) {
+        $path = $configPath . '/SprayFire/environment.php';
+        $config = array();
+        if (\file_exists($path)) {
+            $config = (array) include $path;
+        }
+
+        return new \SprayFire\EnvironmentConfig($config);
+    };
+
+    $EnvironmentConfig = $getEnvironmentConfig();
+
+    $RootPaths = new FireFileSys\RootPaths($installPath, $libsPath, $appPath, $webPath, $configPath, $logsPath);
+    $Paths = new FireFileSys\Paths($RootPaths, $EnvironmentConfig->useVirtualHost());
+
+    $JavaNameConverter = new SFUtils\JavaNamespaceConverter();
+    $ReflectionCache = new SFUtils\ReflectionCache($JavaNameConverter);
+    $Container = new FireService\Container($ReflectionCache);
+
+    $getRouteBag = function() use ($Paths) {
+        $Bag = include $Paths->getConfigPath('SprayFire', 'routes.php');
+        if (!$Bag instanceof \SprayFire\Http\Routing\RouteBag) {
+            $Bag = new FireRouting\RouteBag();
+        }
+        return $Bag;
+    };
+
+    /**
+     * We are instantiating these services here to prevent unneeded reflection for
+     * components of the framework that are highly unlikely to change. This drastically
+     * improves processing time and memory used.
+     */
+
+    $EmergencyLogger = new FireLogging\SysLogLogger();
+    $ErrorLogger = new FireLogging\ErrorLogLogger();
+    $InfoLogger = new FireLogging\DevelopmentLogger();
+    $DebugLogger = new FireLogging\DevelopmentLogger();
+    $LogOverseer = new FireLogging\LogOverseer($EmergencyLogger, $ErrorLogger, $DebugLogger, $InfoLogger);
+
+    $Handler = new \SprayFire\Handler($LogOverseer, $EnvironmentConfig->isDevelopmentMode());
+    \set_error_handler(array($Handler, 'trapError'));
+    \set_exception_handler(array($Handler, 'trapException'));
+
+    $Uri = new FireHttp\Uri();
+    $Headers = new FireHttp\RequestHeaders();
+    $Request = new FireHttp\Request($Uri, $Headers);
+
+    $installDir = $EnvironmentConfig->useVirtualHost() ? null : \basename($Paths->getInstallPath());
+    $Strategy = new FireRouting\ConfigurationMatchStrategy($installDir);
+    $RouteBag = $getRouteBag();
+    $Normalizer = new FireRouting\Normalizer();
+    $Router = new FireRouting\Router($Strategy, $RouteBag, $Normalizer);
+    $RoutedRequest = $Router->getRoutedRequest($Request);
+
+    $CallbackStorage = new FireMediator\CallbackStorage();
+    $EventRegistry = new FireMediator\EventRegistry($CallbackStorage);
+    $Mediator = new FireMediator\Mediator($EventRegistry, $CallbackStorage);
+
+    $OutputEscaper = new FireResponder\OutputEscaper($EnvironmentConfig->getDefaultCharset());
+    $TemplateManager = new FireTemplate\Manager();
+
+    $ControllerFactory = new FireController\Factory($ReflectionCache, $Container, $LogOverseer);
+    $ResponderFactory = new FireResponder\Factory($ReflectionCache, $Container, $LogOverseer);
+
+    $Container->addService($Request);
+    $Container->addService($ClassLoader);
+    $Container->addService($Paths);
+    $Container->addService($JavaNameConverter);
+    $Container->addService($ReflectionCache);
+    $Container->addService($EventRegistry);
+    $Container->addService($Mediator);
+    $Container->addService($RoutedRequest);
+    $Container->addService($OutputEscaper);
+    $Container->addService($TemplateManager);
+    $Container->addService($LogOverseer);
+    $Container->addService($EnvironmentConfig);
+
+    foreach ($EnvironmentConfig->getRegisteredEvents() as $eventName => $eventType) {
+        $EventRegistry->registerEvent($eventName, $eventType);
     }
 
-    return new \SprayFire\EnvironmentConfig($config);
-};
+    $AppInitializer = new FireDispatcher\AppInitializer($Container, $Paths, $ClassLoader);
+    $AppInitializer->initializeApp($RoutedRequest);
 
-$EnvironmentConfig = $getEnvironmentConfig();
+    $Dispatcher = new FireDispatcher\Dispatcher($Mediator, $ControllerFactory, $ResponderFactory);
+    $Dispatcher->dispatchResponse($RoutedRequest);
 
-$RootPaths = new FireFileSys\RootPaths($installPath, $libsPath, $appPath, $webPath, $configPath, $logsPath);
-$Paths = new FireFileSys\Paths($RootPaths, $EnvironmentConfig->useVirtualHost());
-
-$JavaNameConverter = new SFUtils\JavaNamespaceConverter();
-$ReflectionCache = new SFUtils\ReflectionCache($JavaNameConverter);
-$Container = new FireService\Container($ReflectionCache);
-
-$getRouteBag = function() use ($Paths) {
-    $Bag = include $Paths->getConfigPath('SprayFire', 'routes.php');
-    if (!$Bag instanceof \SprayFire\Http\Routing\RouteBag) {
-        $Bag = new FireRouting\RouteBag();
+    if ($EnvironmentConfig->isDevelopmentMode()) {
+        echo '<pre>Request time ', (\microtime(true) - $requestStartTime), '</pre>';
+        echo '<pre>Memory usage ', \memory_get_peak_usage(true) / 1048576, ' MB</pre>';
     }
-    return $Bag;
-};
-
-/**
- * We are instantiating these services here to prevent unneeded reflection for
- * components of the framework that are highly unlikely to change. This drastically
- * improves processing time and memory used.
- */
-
-$EmergencyLogger = new FireLogging\SysLogLogger();
-$ErrorLogger = new FireLogging\ErrorLogLogger();
-$InfoLogger = new FireLogging\DevelopmentLogger();
-$DebugLogger = new FireLogging\DevelopmentLogger();
-$LogOverseer = new FireLogging\LogOverseer($EmergencyLogger, $ErrorLogger, $DebugLogger, $InfoLogger);
-
-$Handler = new \SprayFire\Handler($LogOverseer, $EnvironmentConfig->isDevelopmentMode());
-\set_error_handler(array($Handler, 'trapError'));
-\set_exception_handler(array($Handler, 'trapException'));
-
-$Uri = new FireHttp\Uri();
-$Headers = new FireHttp\RequestHeaders();
-$Request = new FireHttp\Request($Uri, $Headers);
-
-$installDir = $EnvironmentConfig->useVirtualHost() ? null : \basename($Paths->getInstallPath());
-$Strategy = new FireRouting\ConfigurationMatchStrategy($installDir);
-$RouteBag = $getRouteBag();
-$Normalizer = new FireRouting\Normalizer();
-$Router = new FireRouting\Router($Strategy, $RouteBag, $Normalizer);
-$RoutedRequest = $Router->getRoutedRequest($Request);
-
-$CallbackStorage = new FireMediator\CallbackStorage();
-$EventRegistry = new FireMediator\EventRegistry($CallbackStorage);
-$Mediator = new FireMediator\Mediator($EventRegistry, $CallbackStorage);
-
-$OutputEscaper = new FireResponder\OutputEscaper($EnvironmentConfig->getDefaultCharset());
-$TemplateManager = new FireTemplate\Manager();
-
-$ControllerFactory = new FireController\Factory($ReflectionCache, $Container, $LogOverseer);
-$ResponderFactory = new FireResponder\Factory($ReflectionCache, $Container, $LogOverseer);
-
-$Container->addService($Request);
-$Container->addService($ClassLoader);
-$Container->addService($Paths);
-$Container->addService($JavaNameConverter);
-$Container->addService($ReflectionCache);
-$Container->addService($EventRegistry);
-$Container->addService($Mediator);
-$Container->addService($RoutedRequest);
-$Container->addService($OutputEscaper);
-$Container->addService($TemplateManager);
-$Container->addService($LogOverseer);
-$Container->addService($EnvironmentConfig);
-
-foreach ($EnvironmentConfig->getRegisteredEvents() as $eventName => $eventType) {
-    $EventRegistry->registerEvent($eventName, $eventType);
 }
 
-$AppInitializer = new FireDispatcher\AppInitializer($Container, $Paths, $ClassLoader);
-$AppInitializer->initializeApp($RoutedRequest);
+startProcessing();
 
-$Dispatcher = new FireDispatcher\Dispatcher($Mediator, $ControllerFactory, $ResponderFactory);
-$Dispatcher->dispatchResponse($RoutedRequest);
 
-if ($EnvironmentConfig->isDevelopmentMode()) {
-    echo '<pre>Request time ', (\microtime(true) - $requestStartTime), '</pre>';
-    echo '<pre>Memory usage ', \memory_get_peak_usage(true) / 1048576, ' MB</pre>';
-}
