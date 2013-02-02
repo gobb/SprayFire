306282127f2c57756e7a703ba116e6a5644d62d0
Charles Sprayberry <cspray@gmail.com>
2013-02-01 20:27:46 -0500

    ADD abstract class for plugins, apps and optional modules to add appropriate services and autoloading

---
+++ libs/SprayFire/Bootstrap/FireBootstrap/Pluggable.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Abstract \SprayFire\Bootstrap\Bootstrapper that provides easy access to service
+ * containers and autoloading setup for apps, plugins and optional SprayFire modules.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.1
+ * @since   0.1
+ */
+namespace SprayFire\Bootstrap\FireBootstrap;
+
+use \SprayFire\Bootstrap as SFBootstrap,
+    \SprayFire\Service as SFService,
+    \SprayFire\CoreObject as SFCoreObject,
+    \ClassLoader\Loader as ClassLoader;
+
+/**
+ * @package SprayFire
+ * @subpackage Bootstrap.FireBootstrap
+ */
+abstract class Pluggable extends SFCoreObject implements SFBootstrap\Bootstrapper {
+
+    /**
+     * Container used by framework to hold your services; add services that your
+     * application needs when calling runBootstrap().
+     *
+     * @property \SprayFire\Service\Container
+     */
+    protected $Container;
+
+    /**
+     * ClassLoader library that allows you to setup autoloading for whatever third
+     * party libraries your app may be using.
+     *
+     * @property \ClassLoader\Loader
+     */
+    protected $ClassLoader;
+
+    /**
+     * @param \SprayFire\Service\Container $Container
+     * @param \ClassLoader\Loader $ClassLoader
+     */
+    public function __construct(SFService\Container $Container, ClassLoader $ClassLoader) {
+        $this->Container = $Container;
+        $this->ClassLoader = $ClassLoader;
+    }
+
+}
