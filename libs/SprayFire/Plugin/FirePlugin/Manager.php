25ee121ceda722fee37e659832a4dbeaaa998bdb
Charles Sprayberry <cspray@gmail.com>
2013-03-03 22:40:25 -0500

    ADD initial FirePlugin\Manager to get tests running

---
+++ libs/SprayFire/Plugin/FirePlugin/Manager.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Framework provided implementation of \SprayFire\Plugin\Manager that will setup
+ * autoloading for a plugin and ensure the callbacks associated to that plugin are
+ * added to the Mediator.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.2
+ * @since   0.2
+ */
+namespace SprayFire\Plugin\FirePlugin;
+
+use \SprayFire\Plugin as SFPlugin,
+    \SprayFire\StdLib as SFStdLib;
+
+/**
+ * @package SprayFire
+ * @subpackage Plugin.FirePlugin
+ */
+class Manager extends SFStdLib\CoreObject implements SFPlugin\Manager {
+
+    /**
+     * Registering a plugin should involve any tasks the plugin needs to be setup
+     * and start working.
+     *
+     * At minimum the following should occur:
+     * - Set up the plugin for autoloading
+     * - Add any callbacks associated with the plugin to the Mediator
+     *
+     * @param \SprayFire\Plugin\PluginSignature $PluginSignature
+     * @return boolean
+     */
+    public function registerPlugin(SFPlugin\PluginSignature $PluginSignature) {
+        // TODO: Implement registerPlugin() method.
+    }
+
+    /**
+     * The $plugins parameter should be a structure containing \SprayFire\Plugin\PluginSignature[].
+     *
+     * An exception should be thrown when an element in $plugins does not properly
+     * implement the PluginSignature interface.
+     *
+     * @param array|Traversable $plugins
+     * @return boolean
+     * @throws \InvalidArgumentException
+     */
+    public function registerPlugins($plugins) {
+        // TODO: Implement registerPlugins() method.
+    }
+
+    /**
+     * Will return a collection of \SprayFire\Plugin\PluginSignature implementations
+     * that have been registered so far.
+     *
+     * @return array|Traversable
+     */
+    public function getRegisteredPlugins() {
+        // TODO: Implement getRegisteredPlugins() method.
+    }
+    
+}
