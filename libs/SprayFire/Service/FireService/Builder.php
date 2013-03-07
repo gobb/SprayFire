f4713b55e7f1e589fbded378c42f2314ecbe55a5
Charles Sprayberry <cspray@gmail.com>
2013-03-06 23:52:18 -0500

    ADD concrete implementation of Service\Builder

---
+++ libs/SprayFire/Service/FireService/Builder.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * An implementation of SprayFire\Service\Builder that allows you to easily
+ * give services to Consumers.
+ *
+ * @author  Charles Sprayberry
+ * @license Subject to the terms of the LICENSE file in the project root
+ * @version 0.2
+ * @since   0.2
+ */
+namespace SprayFire\Service\FireService;
+
+use \SprayFire\Service as SFService,
+    \SprayFire\StdLib as SFStdLib;
+
+/**
+ * Provides a concrete implementation to build services needed for a Service\Consumer.
+ *
+ * @package SprayFire
+ * @subpackage Service.FireService
+ */
+class Builder extends SFStdLib\CoreObject implements SFService\Builder {
+
+    /**
+     * @property \SprayFire\Service\Container
+     */
+    protected $Container;
+
+    /**
+     * @param \SprayFire\Service\Container $Container
+     */
+    public function __construct(SFService\Container $Container) {
+        $this->Container = $Container;
+    }
+
+    /**
+     * Adds the services requested to the $Consumer passed.
+     *
+     * This implementation does not catch the exceptions thrown by
+     * \SprayFire\Service\Container.
+     *
+     * @param \SprayFire\Service\Consumer $Consumer
+     * @return void
+     * @throws \SprayFire\Service\Exception\ServiceNotFound
+     * @throws \SprayFire\Service\Exception\FactoryNotRegistered
+     */
+    public function buildConsumer(SFService\Consumer $Consumer) {
+        $services = $Consumer->getRequestedServices();
+        if ($this->isTraversable($services)) {
+            foreach ($services as $property => $service) {
+                $ServiceObject = $this->Container->getService($service);
+                $Consumer->giveService($property, $ServiceObject);
+            }
+        }
+    }
+
+    /**
+     * Used to ensure that the values returned from Consumer::getRequestedServices
+     * can be properly iterated over.
+     *
+     * @param mixed $type
+     * @return boolean
+     */
+    protected function isTraversable($type) {
+        if (\is_array($type) || $type instanceof \Traversable || $type instanceof \stdClass) {
+            return true;
+        }
+        // @codeCoverageIgnoreStart
+        // No reliable way to test this, if this is returned no services added to consumer
+        return false;
+        // @codeCoverageIgnoreEnd
+    }
+
+}
